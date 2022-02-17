23b6429c03ad222872c8fc9c57fca5b5922d01b3
faithwang <wangyanteng@huawei.com>
2022-02-17 17:48:38 +0800

    fix codex
    Signed-off-by: faithwang <wangyanteng@huawei.com>

--- adapter/utility/file_util.cpp
+++ adapter/utility/file_util.cpp
@@ -292,7 +292,7 @@ bool IsDirectory(const std::string &path)
     return fs::is_directory(path);
 }
 
-bool GetLastLine(std::istream &fin, std::string &line, int maxLen)
+bool GetLastLine(std::istream &fin, std::string &line, uint32_t maxLen)
 {
     if (fin.tellg() <= 0) {
         return false;
