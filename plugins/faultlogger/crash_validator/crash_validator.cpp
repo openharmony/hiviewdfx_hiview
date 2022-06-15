6fd692a6f2540f5fced74b9702ced85731e4dc7a
liupeng298@huawei.com <liupeng298@huawei.com>
2022-06-15 10:30:12 +0800

    类型不匹配报错 Signed-off-by:liupeng298<liupeng298@huawei.com>
    Signed-off-by: liupeng298@huawei.com <liupeng298@huawei.com>

--- plugins/faultlogger/crash_validator/crash_validator.cpp
+++ plugins/faultlogger/crash_validator/crash_validator.cpp
@@ -66,7 +66,7 @@ void CrashValidator::PrintEvents(int fd, const std::vector<CrashEvent>& events)
 {
     std::vector<CrashEvent>::const_iterator it = events.begin(); 
     while (it != events.end()) {
-        dprintf(fd, "Module:%s Time:%llu Pid:%llu Uid:%llu HasLog:%d\n",
+        dprintf(fd, "Module:%s Time:%" PRIu64" Pid:%" PRIu64" Uid:%" PRIu64" HasLog:%d\n",
             it->name.c_str(),
             static_cast<unsigned long long>(it->time),
             it->pid,
