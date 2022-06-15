a44b5c43b77f7754728a202abddd359f52e3286c
mgceshuang <mashuang13@huawei.com>
2022-06-15 11:05:29 +0800

    add oem build
    Signed-off-by: mgceshuang <mashuang13@huawei.com>

--- build/run_oem_hook.py
+++ build/run_oem_hook.py
@@ -12,13 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import argparse
 import os
 import runpy
 import sys
 
-
 DEBUG = False
-
 
 def find_oem_script(path):
     for relpath, dirs, files in os.walk(path):
@@ -31,13 +30,36 @@ def find_oem_script(path):
 def main():
     if DEBUG:
         print("Run oem hook start")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--out_build_file', help='oem build', required=True)
+    args = parser.parse_args()
+    output_file = args.out_build_file
+    if DEBUG:
+        print("output_file:" + output_file)
+
     hiview_build_path = os.path.split(os.path.realpath(__file__))[0]
     hiview_path = os.path.realpath(os.path.join(hiview_build_path, ".."))
     script_path = find_oem_script(hiview_path)
+    globals_args = {'oem_dynamic_deps': [], 'oem_test_deps': []}
     if script_path.strip() == '':
         print("Could not find oem hook")
     else:
-        runpy.run_path(script_path, run_name='__main__')
+        runpy.run_path(script_path, init_globals=globals_args ,run_name='__main__')
+    if DEBUG:
+        print(globals_args)
+
+    dynamic_info = 'oem_dynamic_deps = [\n%s\n]\n' % (''.join(globals_args.get('oem_dynamic_deps', '')))
+    test_info = ('oem_test_deps = [\n%s\n]\n' % (''.join(globals_args.get('oem_test_deps', ''))))
+    buff = '%s%s' % (dynamic_info, test_info)
+    out_dir = os.path.dirname(output_file)
+    print('out_dir:' + out_dir)
+    if os.path.isdir(out_dir) is False:
+        os.makedirs(out_dir)
+    with open(output_file, 'w') as file_id:
+        if DEBUG:
+            print(buff)
+        file_id.write(buff)
 
     if DEBUG:
         print("Run oem hook end")
