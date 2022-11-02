5f57329f0c63255a3caf8771cbacf323df56e84b
xuyong <xuyong59@huawei.com>
2022-11-02 14:44:21 +0800

    fixed 559aef5 from https://gitee.com/honghecun/hiviewdfx_hiview/pulls/460 修复查询管控外部查询错误记录以及内部查询condition计算方式与外部查询同步的bug
    Signed-off-by: xuyong <xuyong59@huawei.com>

--- adapter/dbstore/data_query.cpp
+++ adapter/dbstore/data_query.cpp
@@ -20,7 +20,7 @@
 namespace OHOS {
 namespace HiviewDFX {
 int DataQuery::defLimit_ = 100;
-DataQuery::DataQuery() : limit_(defLimit_), offset_(0), conditionCnt(0) {}
+DataQuery::DataQuery() : limit_(defLimit_), offset_(0) {}
 
 DataQuery::~DataQuery() {}
 
@@ -42,14 +42,12 @@ DOCSTORE_API DataQuery& DataQuery::Select(const std::vector<std::string>& fields
 DataQuery& DataQuery::StartWith(const std::string &field, const std::string &value)
 {
     sql_ << "/[" << field << " re \"" << value << "\"]";
-    conditionCnt++;
     return *this;
 }
 
 DataQuery& DataQuery::NotStartWith(const std::string &field, const std::string &value)
 {
     sql_ << "/[" << field << " not re \"" << value << ".*\"]";
-    conditionCnt++;
     return *this;
 }
 
@@ -139,11 +137,6 @@ std::string DataQuery::ToDelString(int limit) const
     } else {
         return sql + " | del | asc /time_ limit " + std::to_string(limit) + " count";
     }
-}
-
-int DataQuery::GetConditionCnt() const
-{
-    return conditionCnt;
 }
 } // HiviewDFX
 } // OHOS
