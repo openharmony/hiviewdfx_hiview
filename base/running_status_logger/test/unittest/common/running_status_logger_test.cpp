0d254be78f6285fa794381e3a865f4355d0a049e
xuyong <xuyong59@huawei.com>
2022-12-02 16:12:17 +0800

    fixed 388e134 from https://gitee.com/honghecun/hiviewdfx_hiview/pulls/568 测试用例补齐
    Signed-off-by: xuyong <xuyong59@huawei.com>

--- base/running_status_logger/test/unittest/common/running_status_logger_test.cpp
+++ base/running_status_logger/test/unittest/common/running_status_logger_test.cpp
@@ -21,10 +21,24 @@
 #include <memory>
 #include <thread>
 
+#include "file_util.h"
+#include "hiview_global.h"
+#include "plugin.h"
 #include "running_status_logger.h"
 
 namespace OHOS {
 namespace HiviewDFX {
+namespace {
+constexpr char TEST_LOG_DIR[] = "/data/log/hiview/sys_event_test";
+
+class HiviewTestContext : public HiviewContext {
+public:
+    std::string GetHiViewDirectory(DirectoryType type __UNUSED)
+    {
+        return TEST_LOG_DIR;
+    }
+};
+}
 void RunningStatusLoggerTest::SetUpTestCase()
 {
 }
@@ -42,8 +56,35 @@ void RunningStatusLoggerTest::TearDown()
 }
 
 /**
- * @tc.name: RunningStatusLoggerTest_002
+ * @tc.name: RunningStatusLoggerTest_001
  * @tc.desc: write log to file
+ * @tc.type: FUNC
+ * @tc.require: issueI62PQY
+ */
+HWTEST_F(RunningStatusLoggerTest, RunningStatusLoggerTest_001, testing::ext::TestSize.Level3)
+{
+    /**
+     * @tc.steps: step1. init hiview global with customized hiview context
+     * @tc.steps: step2. init string with 4k size
+     * @tc.steps: step3. log string to log file until the size of this log file over limit 2M.
+     */
+    HiviewTestContext hiviewTestContext;
+    HiviewGlobal::CreateInstance(hiviewTestContext);
+    std::string singleLine = "line";
+    for (int index = 0; index < 1024; index++) {
+        singleLine += "line";
+    }
+    for (int index = 0; index < 1024; index++) {
+        RunningStatusLogger::GetInstance().Log(singleLine);
+    }
+    ASSERT_TRUE(true);
+    FileUtil::ForceRemoveDirectory(TEST_LOG_DIR);
+    ASSERT_TRUE(true);
+}
+
+/**
+ * @tc.name: RunningStatusLoggerTest_002
+ * @tc.desc: time stamp format
  * @tc.type: FUNC
  * @tc.require: issueI62PQY
  */
@@ -55,6 +96,5 @@ HWTEST_F(RunningStatusLoggerTest, RunningStatusLoggerTest_002, testing::ext::Tes
     ASSERT_TRUE(!normalTimeStamp.empty() && normalTimeStamp.find(':') != std::string::npos &&
         normalTimeStamp.find('/') != std::string::npos);
 }
-
 } // namespace HiviewDFX
 } // namespace OHOS
