07d07175df93b322b86445676fdae86f96d1fbd5
yangshiqi <670097973@qq.com>
2022-12-06 17:50:24 +0800

    fixed 4fac4f6 from https://gitee.com/yang-shiqi-hw/hiviewdfx_hiview/pulls/578 add TDD testCase
    Signed-off-by: yangshiqi <670097973@qq.com>

--- plugins/eventservice/test/unittest/common/event_service_action_test.cpp
+++ plugins/eventservice/test/unittest/common/event_service_action_test.cpp
@@ -31,25 +31,35 @@
 #include "sys_event_dao.h"
 #include "sys_event_db_backup.h"
 #include "sys_event_service.h"
+#include "flat_json_parser.h"
 
 namespace OHOS {
 namespace HiviewDFX {
-void EventServiceActionTest::SetUpTestCase()
+void EventServiceActionTest::SetUpTestCase() {}
+
+void EventServiceActionTest::TearDownTestCase() {}
+
+void EventServiceActionTest::SetUp()
 {
-    OHOS::HiviewDFX::HiviewPlatform &platform = HiviewPlatform::GetInstance();
+    platform_ =  std::make_shared<HiviewPlatform>();
     std::string defaultDir = "/data/test/test_data/hiview_platform_config";
-    if (!platform.InitEnvironment(defaultDir)) {
+    if (!platform_->InitEnvironment(defaultDir)) {
         std::cout << "fail to init environment" << std::endl;
     } else {
         std::cout << "init environment successful" << std::endl;
     }
+    sysEventDbMgrPtr = std::make_unique<SysEventDbMgr>();
+    currentLooper_ = std::make_shared<EventLoop>("EventServiceActionTest");
+    currentLooper_->StartLoop();
 }
 
-void EventServiceActionTest::TearDownTestCase() {}
-
-void EventServiceActionTest::SetUp() {}
-
-void EventServiceActionTest::TearDown() {}
+void EventServiceActionTest::TearDown()
+{
+    if (currentLooper_ != nullptr) {
+        currentLooper_->StopLoop();
+        currentLooper_.reset();
+    }
+}
 
 /**
  * @tc.name: EventJsonParserTest001
@@ -68,6 +78,20 @@ HWTEST_F(EventServiceActionTest, EventJsonParserTest001, testing::ext::TestSize.
     yamlFile = (yamlFile.back() != '/') ? (yamlFile + "/hisysevent.def") : (yamlFile + "hisysevent.def");
     auto sysEventParser = std::make_unique<EventJsonParser>(yamlFile);
     ASSERT_TRUE(sysEventParser->HandleEventJson(sysEvent));
+    ASSERT_TRUE(sysEventParser->GetTagByDomainAndName("abc", "abc") == "");
+    ASSERT_TRUE(sysEventParser->GetTagByDomainAndName("DEMO", "abc") == "");
+    ASSERT_TRUE(sysEventParser->GetTypeByDomainAndName("DEMO", "abc") == 0);
+    DuplicateIdFilter filter;
+    filter.IsDuplicateEvent("aaa");
+    filter.IsDuplicateEvent("a1");
+    filter.IsDuplicateEvent("a2");
+    filter.IsDuplicateEvent("a3");
+    filter.IsDuplicateEvent("a4");
+    filter.IsDuplicateEvent("a5");
+    ASSERT_TRUE(filter.IsDuplicateEvent("a5"));
+    FlatJsonParser parser("{\"level\":[abc]}");
+    std::cout << "FlatJsonParser:" << parser.Print() << std::endl;
+    ASSERT_TRUE(parser.Print()=="{\"level\":[abc]}");
 }
 
 int OpenTestFile(const char *filename)
@@ -136,10 +160,53 @@ HWTEST_F(EventServiceActionTest, SysEventStatTest002, testing::ext::TestSize.Lev
 }
 
 /**
- * @tc.name: SysEventService
+ * @tc.name: SysEventDao003
+ * @tc.desc: check event Dao
+ * @tc.type: FUNC
+ * @tc.require:issueI64VNS
+ */
+HWTEST_F(EventServiceActionTest, SysEventDao003, testing::ext::TestSize.Level3)
+{
+    std::string jsonStr1 = R"~({"domain_":"demo","name_":"SysEventDaoTest_003","type_":1,"tz_":8,"time_":162027129110,
+        "pid_":6527,"tid_":6527,"traceid_":"f0ed5160bb2df4b","spanid_":"10","pspanid_":"20","trace_flag_":4,
+        "keyBool":1})~";
+    auto sysEvent = std::make_shared<SysEvent>("SysEventSource", nullptr, jsonStr1);
+    std::cout <<" SysEventDao003:" << 0 << std::endl;
+    ASSERT_TRUE(sysEvent->ParseJson() == 0);
+    sysEventDbMgrPtr->SaveToStore(sysEvent);
+
+    auto sysEventQuery = EventStore::SysEventDao::BuildQuery(EventStore::StoreType::FAULT);
+    std::vector<std::string> selections { EventStore::EventCol::NAME };
+    EventStore::ResultSet resultSet = (*sysEventQuery).Select(selections).
+        Where(EventStore::EventCol::NAME, EventStore::Op::EQ, "SysEventDaoTest_003").Execute();
+    int count = 0;
+    while (resultSet.HasNext()) {
+        EventStore::ResultSet::RecordIter it = resultSet.Next();
+        std::cout << "seq=" << it->GetSeq() << std::endl;
+        count++;
+    }
+    ASSERT_TRUE(count > 0);
+    std::cout <<" count:" << count << std::endl;
+    for (int i = 0; i < 50; i++) {
+        SysEventCreator sysEventCreator("domain1", "test1", SysEventCreator::FAULT);
+        sysEventCreator.SetKeyValue("KEY_INT", i);
+        auto sysEvent1 = std::make_shared<SysEvent>("SysEventSource", nullptr, sysEventCreator);
+        EventStore::SysEventDao::Insert(sysEvent1);
+    }
+    auto testPlugin = std::make_shared<SysEventService>();
+    sysEventDbMgrPtr->StartCheckStoreTask(nullptr);
+    sysEventDbMgrPtr->StartCheckStoreTask(currentLooper_);
+    sysEventDbMgrPtr->CheckStore();
+    SysEventDbBackup dbBackup(EventStore::StoreType::FAULT);
+    ASSERT_TRUE(dbBackup.IsBroken() == 0);
+    ASSERT_TRUE(dbBackup.Recover() == 1);
+}
+
+/**
+ * @tc.name: SysEventService004
  * @tc.desc: check sysEvent service
  * @tc.type: FUNC
- * @tc.require: issueI62WJT
+ * @tc.require:issueI64VNS
  */
 HWTEST_F(EventServiceActionTest, SysEventService004, testing::ext::TestSize.Level3)
 {
