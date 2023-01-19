794e1c8a1410db33c58dea8ecc62b052628f1039
lyj_love_code <liangyujian2@huawei.com>
2023-01-19 14:16:00 +0800

    optimize memory for hiview thread
    Signed-off-by: lyj_love_code <liangyujian2@huawei.com>

--- base/event_loop.cpp
+++ base/event_loop.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -34,6 +34,7 @@
 #include "audit.h"
 #include "file_util.h"
 #include "logger.h"
+#include "memory_util.h"
 #include "thread_util.h"
 namespace OHOS {
 namespace HiviewDFX {
@@ -393,6 +394,10 @@ void EventLoop::PollNextEvent(uint64_t timeout)
 
 void EventLoop::Run()
 {
+    if (MemoryUtil::DisableThreadCache() != 0 || MemoryUtil::DisableDelayFree() != 0) {
+        HIVIEW_LOGW("Failed to optimize memory for current thread");
+    }
+
     // set thread name
     const int maxLength = 16;
     std::string restrictedName = name_;
