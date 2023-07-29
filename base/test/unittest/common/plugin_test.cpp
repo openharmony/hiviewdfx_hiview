4048eac43c6b26c11cd6009f1a6efc2336994f73
lyj_love_code <liangyujian2@huawei.com>
2023-07-29 15:19:53 +0800

    add testcase for plugin.h
    Signed-off-by: lyj_love_code <liangyujian2@huawei.com>

---
+++ base/test/unittest/common/plugin_test.cpp
@@ -0,0 +1,174 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "plugin_test.h"
+
+#include "plugin.h"
+#include "pipeline.h"
+
+namespace OHOS {
+namespace HiviewDFX {
+void PluginTest::SetUpTestCase()
+{
+}
+
+void PluginTest::TearDownTestCase()
+{
+}
+
+void PluginTest::SetUp()
+{
+}
+
+void PluginTest::TearDown()
+{
+}
+
+/**
+ * @tc.name: PluginTest001
+ * @tc.desc: Test the api of Plugin.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(PluginTest, PluginTest001, testing::ext::TestSize.Level0)
+{
+    /**
+     * @tc.steps: step1. create Plugin object.
+     * @tc.steps: step2. invoke the function of the plugin object.
+     */
+    printf("PluginTest001 start\n");
+    Plugin plugin;
+    ASSERT_FALSE(plugin.HasLoaded());
+
+    plugin.OnLoad();
+    auto event = plugin.GetEvent(Event::SYS_EVENT);
+    ASSERT_NE(event, nullptr);
+    ASSERT_TRUE(plugin.OnEvent(event));
+
+    /* default null function test */
+    // Dump
+    plugin.Dump(0, {});
+    // OnEventListeningCallback
+    plugin.OnEventListeningCallback(*(event.get()));
+
+    // delay to handle event
+    auto pipelineEvent = std::make_shared<PipelineEvent>(*(event.get()));
+    ASSERT_FALSE(pipelineEvent->hasPending_);
+    auto workLoop = std::make_shared<EventLoop>("testLoop");
+    plugin.BindWorkLoop(workLoop);
+    ASSERT_NE(plugin.GetWorkLoop(), nullptr);
+    plugin.DelayProcessEvent(pipelineEvent, 1); // 1s delay
+    ASSERT_TRUE(pipelineEvent->hasPending_);
+    sleep(3);
+
+    // udpate the active time
+    auto lastActiveTime1 = plugin.GetLastActiveTime();
+    ASSERT_GT(lastActiveTime1, 0);
+    plugin.UpdateTimeByDelay(1000); // delay 1s
+    auto lastActiveTime2 = plugin.GetLastActiveTime();
+    ASSERT_GT(lastActiveTime2, lastActiveTime1);
+
+    // set/get function
+    std::string version = "1.0";
+    plugin.SetVersion(version);
+    ASSERT_EQ(version, plugin.GetVersion());
+
+    // bundle test
+    ASSERT_FALSE(plugin.IsBundlePlugin());
+    std::string bundleName = "testBundle";
+    plugin.SetBundleName(bundleName);
+    ASSERT_TRUE(plugin.IsBundlePlugin());
+
+    plugin.OnUnload();
+    printf("PluginTest001 end\n");
+}
+
+/**
+ * @tc.name: HiviewContextTest001
+ * @tc.desc: Test the api of HiviewContext.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(PluginTest, HiviewContextTest001, testing::ext::TestSize.Level0)
+{
+    /**
+     * @tc.steps: step1. create HiviewContext object.
+     * @tc.steps: step2. invoke the function of the HiviewContext object.
+     */
+    printf("HiviewContextTest001 start\n");
+    HiviewContext context;
+
+    /* default null function test */
+    // PostUnorderedEvent
+    context.PostUnorderedEvent(nullptr, nullptr);
+    // RegisterUnorderedEventListener
+    std::weak_ptr<EventListener> eventListenerPtr;
+    context.RegisterUnorderedEventListener(eventListenerPtr);
+    // PostAsyncEventToTarget
+    context.PostAsyncEventToTarget(nullptr, "", nullptr);
+    // RequestUnloadPlugin
+    context.RequestUnloadPlugin(nullptr);
+    // PublishPluginCapacity
+    PluginCapacityInfo capInfo = { "", {}, "", {} };
+    context.PublishPluginCapacity(capInfo);
+    // GetRemoteByCapacity
+    std::string plugin;
+    std::string capacity;
+    std::list<std::string> deviceIdList;
+    context.GetRemoteByCapacity(plugin, capacity, deviceIdList);
+    // AppendPluginToPipeline
+    context.AppendPluginToPipeline("", "");
+    // RequestLoadBundle
+    context.RequestLoadBundle("");
+    // RequestUnloadBundle
+    context.RequestUnloadBundle("");
+    // AddListenerInfo
+    context.AddListenerInfo(0, "", {}, {});
+    context.AddListenerInfo(0, "");
+    // AddDispatchInfo
+    std::weak_ptr<Plugin> pluginPtr;
+    context.AddDispatchInfo(pluginPtr, {}, {}, {}, {});
+
+    /* default null function test with return value */
+    // PostSyncEventToTarget
+    ASSERT_TRUE(context.PostSyncEventToTarget(nullptr, "", nullptr));
+    // PostEventToRemote
+    ASSERT_EQ(context.PostEventToRemote(nullptr, "", "",  nullptr), 0);
+    // GetSharedWorkLoop
+    ASSERT_EQ(context.GetSharedWorkLoop(), nullptr);
+    // GetPipelineSequenceByName
+    ASSERT_TRUE(context.GetPipelineSequenceByName("").empty());
+    // IsReady
+    ASSERT_FALSE(context.IsReady());
+    // GetHiViewDirectory
+    ASSERT_TRUE(context.GetHiViewDirectory(HiviewContext::DirectoryType::CONFIG_DIRECTORY).empty());
+    ASSERT_TRUE(context.GetHiViewDirectory(HiviewContext::DirectoryType::WORK_DIRECTORY).empty());
+    ASSERT_TRUE(context.GetHiViewDirectory(HiviewContext::DirectoryType::PERSIST_DIR).empty());
+    // GetHiviewProperty
+    ASSERT_EQ(context.GetHiviewProperty("test_key", "test_value"), "test_value");
+    // SetHiviewProperty
+    ASSERT_TRUE(context.SetHiviewProperty("test_key", "test_value", true));
+    // InstancePluginByProxy
+    ASSERT_EQ(context.InstancePluginByProxy(nullptr), nullptr);
+    // GetPluginByName
+    ASSERT_EQ(context.GetPluginByName(""), nullptr);
+    // GetListenerInfo
+    ASSERT_TRUE(context.GetListenerInfo(0, "", "").empty());
+    // GetDisPatcherInfo
+    ASSERT_TRUE(context.GetDisPatcherInfo(0, "", "", "").empty());
+
+    printf("HiviewContextTest001 end\n");
+}
+} // namespace HiviewDFX
+} // namespace OHOS
