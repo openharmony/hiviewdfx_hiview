be59eebe091ba4865b4b92120cd47c2cafc8482a
lyj_love_code <liangyujian2@huawei.com>
2023-08-30 12:04:03 +0800

    bug: fix the inner query condition problem
    Signed-off-by: lyj_love_code <liangyujian2@huawei.com>

--- base/event_store/include/doc_query.h
+++ base/event_store/include/doc_query.h
@@ -17,6 +17,8 @@
 
 #include <string>
 
+#include "base_def.h"
+
 namespace OHOS {
 namespace HiviewDFX {
 namespace EventRaw {
@@ -35,20 +37,7 @@ public:
     std::string ToString() const;
 
 private:
-#pragma pack(1)
-    /* for internal field query */
-    struct InnerFieldStruct {
-        int64_t seq;
-        uint64_t ts;
-        uint8_t tz;
-        uint32_t uid;
-        uint32_t pid;
-        uint32_t tid;
-    };
-    using InnerFieldStruct = struct InnerFieldStruct;
-#pragma pack()
-
-    bool IsContainInnerCond(const InnerFieldStruct& innerField, const Cond& cond) const;
+    bool IsContainInnerCond(const DocEventHeader& eventHeader, const Cond& cond) const;
     bool IsContainCond(const Cond& cond, const FieldValue& value) const;
     bool IsInnerCond(const Cond& cond) const;
 
