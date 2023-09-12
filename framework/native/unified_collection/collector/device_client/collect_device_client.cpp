922efcd06e3bc82ed2ac6fe2b91507222a31b5eb
zhouhaifeng <kutcher.zhou@huawei.com>
2023-09-12 21:12:32 +0800

    add unified collection client for collect cpu load
    Signed-off-by: zhouhaifeng <kutcher.zhou@huawei.com>

---
+++ framework/native/unified_collection/collector/device_client/collect_device_client.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "collect_device_client.h"
+
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "logger.h"
+#include "securec.h"
+
+DEFINE_LOG_TAG("UDeviceCli");
+
+#define UNIFIED_COLLECTION_DEVICE  "/dev/ucollection"
+namespace OHOS {
+namespace HiviewDFX {
+CollectDeviceClient::CollectDeviceClient(): fd_(-1)
+{}
+
+CollectDeviceClient::~CollectDeviceClient()
+{
+    if (fd_ > 0) {
+        close(fd_);
+    }
+}
+
+int CollectDeviceClient::GetDeviceFd(bool readOnly)
+{
+    int f = 0;
+    if (readOnly) {
+        f = open(UNIFIED_COLLECTION_DEVICE, O_RDONLY);
+    } else {
+        f = open(UNIFIED_COLLECTION_DEVICE, O_WRONLY);
+    }
+
+    if (f < 0) {
+        HIVIEW_LOGI("Cannot open file=%{public}s, readOnly=%{public}d", UNIFIED_COLLECTION_DEVICE, readOnly);
+        return -1;
+    }
+    HIVIEW_LOGI("open ucollection device readOnly=%{public}d successful", readOnly);
+    return f;
+}
+
+int CollectDeviceClient::Open()
+{
+    fd_ = GetDeviceFd(true);
+    return (fd_ > 0) ? 0 : -1;
+}
+
+std::shared_ptr<ProcessCpuData> CollectDeviceClient::FetchProcessCpuData()
+{
+    HIVIEW_LOGI("send IOCTRL_COLLECT_ALL_PROC_CPU, cmd=%{public}u", IOCTRL_COLLECT_ALL_PROC_CPU);
+    std::shared_ptr<ProcessCpuData> data = std::make_shared<ProcessCpuData>();
+    int ret = ioctl(fd_, IOCTRL_COLLECT_ALL_PROC_CPU, data->entry_);
+    if (ret < 0) {
+        HIVIEW_LOGI("ioctl IOCTRL_COLLECT_ALL_PROC_CPU cmd=%{public}d, ret=%{public}d",
+            IOCTRL_COLLECT_ALL_PROC_CPU, ret);
+        return data;
+    }
+    return data;
+}
+
+std::shared_ptr<ProcessCpuData> CollectDeviceClient::FetchProcessCpuData(int pid)
+{
+    HIVIEW_LOGI("send IOCTRL_COLLECT_THE_PROC_CPU, cmd=%{public}u", IOCTRL_COLLECT_THE_PROC_CPU);
+    std::shared_ptr<ProcessCpuData> data = std::make_shared<ProcessCpuData>(pid);
+    int ret = ioctl(fd_, IOCTRL_COLLECT_THE_PROC_CPU, data->entry_);
+    if (ret < 0) {
+        HIVIEW_LOGI("ioctl IOCTRL_COLLECT_THE_PROC_CPU cmd=%{public}d, ret=%{public}d",
+            IOCTRL_COLLECT_THE_PROC_CPU, ret);
+        return data;
+    }
+    return data;
+}
+
+int CollectDeviceClient::SetDmips(const std::vector<char> &dmipVals)
+{
+    if (dmipVals.empty()) {
+        return 0;
+    }
+    int totalSize = sizeof(struct ucollection_cpu_dmips) + sizeof(char) * dmipVals.size();
+    struct ucollection_cpu_dmips *dmips = (struct ucollection_cpu_dmips *)malloc(totalSize);
+    memset_s(dmips, totalSize, 0, totalSize);
+
+    dmips->magic = IOCTRL_SET_CPU_DMIPS_MAGIC;
+    dmips->total_count = dmipVals.size();
+    for (int ii = 0; ii < dmips->total_count; ii++) {
+        dmips->dmips[ii] = dmipVals[ii];
+    }
+
+    HIVIEW_LOGI("send IOCTRL_SET_CPU_DMIPS, cmd=%{public}u", IOCTRL_SET_CPU_DMIPS);
+    int f = GetDeviceFd(false);
+    int ret = ioctl(f, IOCTRL_SET_CPU_DMIPS, dmips);
+    if (ret < 0) {
+        HIVIEW_LOGI("ioctl IOCTRL_SET_CPU_DMIPS cmd=%{public}u, ret=%{public}d", IOCTRL_SET_CPU_DMIPS, ret);
+        return -1;
+    } else {
+        close(f);
+        HIVIEW_LOGI("set cpu dmips successful");
+    }
+    return 0;
+}
+} // HiviewDFX
+} // OHOS
---
+++ framework/native/unified_collection/collector/inner_include/unified_collection_data.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef FRAMEWORK_NATIVE_UNIFIED_COLLECTION_COLLECTOR_UNIFIED_COLLECTION_DATA
+#define FRAMEWORK_NATIVE_UNIFIED_COLLECTION_COLLECTOR_UNIFIED_COLLECTION_DATA
+
+#include <sys/ioctl.h>
+
+// kernel struct, modify at the same time
+struct ucollection_process_cpu_item {
+    int pid;
+    unsigned long long cpu_usage_utime;
+    unsigned long long cpu_usage_stime;
+    unsigned long long cpu_load_time;
+};
+
+struct ucollection_process_filter {
+    int uid;
+    int pid;
+    int tid;
+};
+
+struct ucollection_process_cpu_entry {
+    int magic;
+    int total_count;
+    int cur_count;
+    struct ucollection_process_filter filter;
+    struct ucollection_process_cpu_item datas[];
+};
+
+struct ucollection_cpu_dmips {
+    int magic;
+    int total_count;
+    char dmips[];
+};
+
+#define IOCTRL_COLLECT_ALL_PROC_CPU_MAGIC 1
+#define IOCTRL_COLLECT_THE_PROC_CPU_MAGIC 1
+#define IOCTRL_SET_CPU_DMIPS_MAGIC 1
+#define DMIPS_NUM 128
+
+#define IOCTRL_COLLECT_CPU_BASE 0
+#define IOCTRL_COLLECT_ALL_PROC_CPU _IOR(IOCTRL_COLLECT_CPU_BASE, 1, struct ucollection_process_cpu_entry)
+#define IOCTRL_COLLECT_THE_PROC_CPU _IOR(IOCTRL_COLLECT_CPU_BASE, 2, struct ucollection_process_cpu_entry)
+#define IOCTRL_SET_CPU_DMIPS _IOW(IOCTRL_COLLECT_CPU_BASE, 3, struct ucollection_cpu_dmips)
+#endif // FRAMEWORK_NATIVE_UNIFIED_COLLECTION_COLLECTOR_UNIFIED_COLLECTION_DATA
