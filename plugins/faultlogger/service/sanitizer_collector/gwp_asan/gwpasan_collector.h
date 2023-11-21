b3ebd07a259b5e121fc7b89fccfa47a386cf8a35
tanding <tanding1@huawei.com>
2023-11-21 18:02:58 +0800

    add libasan_logger.z.so
    Signed-off-by: tanding <tanding1@huawei.com>

---
+++ plugins/faultlogger/service/sanitizer_collector/gwp_asan/gwpasan_collector.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <algorithm>
+#include <map>
+#include <stdint.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+namespace OHOS {
+namespace HiviewDFX {
+struct GwpAsanCurrInfo {
+    /** the time of happening fault */
+    uint64_t happenTime;
+    /** the id of current user when fault happened  */
+    int32_t uid;
+    /** the id of process which fault happened*/
+    int32_t pid;
+    /** type of fault */
+    std::string procName;
+    /** name of module which fault occurred */
+    std::string appVersion;
+    /** the reason why fault occurred */
+    std::string errType;
+    /** the summary of fault information */
+    std::string description;
+    /** information about faultlog using <key,value> */
+    std::map<std::string, std::string> sectionMaps;
+};
+
+void ReadGwpAsanRecord(std::string& gwpAsanBuffer);
+std::string GetApplicationNameById(int32_t uid);
+std::string GetApplicationVersion(int32_t uid, const std::string& bundleName);
+void WriteCollectedData(const GwpAsanCurrInfo &curr_);
+static std::string CalcCollectedLogName(const GwpAsanCurrInfo &curr_);
+static bool WriteNewFile(const int32_t fd, const GwpAsanCurrInfo &curr_);
+static int32_t CreateLogFile(const std::string& name);
+} // namespace HiviewDFX
+} // namespace OHOS
