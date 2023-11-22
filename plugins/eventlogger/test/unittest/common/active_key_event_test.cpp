60fd05080ef58141c745b45d1ca395f57c123013
jessica_2029 <weijiayi2@huawei.com>
2023-11-22 14:21:56 +0800

    fix and add unit testcase of eventlogger
    Signed-off-by: jessica_2029 <weijiayi2@huawei.com>

--- plugins/eventlogger/test/unittest/common/active_key_event_test.cpp
+++ plugins/eventlogger/test/unittest/common/active_key_event_test.cpp
@@ -19,6 +19,8 @@
 #include <vector>
 #include <string>
 
+#define private public
+#define protected public
 #include "active_key_event.h"
 #include "event_thread_pool.h"
 #include "log_store_ex.h"
@@ -44,11 +46,11 @@ void ActiveKeyEventTest::TearDownTestCase()
 }
 
 /**
- * @tc.name: ActiveKeyEventTest
+ * @tc.name: ActiveKeyEventTest_001
  * @tc.desc: ActiveKeyEventTest Init
  * @tc.type: FUNC
  */
-static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest, TestSize.Level3)
+static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest_001, TestSize.Level3)
 {
     std::shared_ptr<EventThreadPool> eventPool = std::make_shared<EventThreadPool>(5, "EventThreadPool");
     EXPECT_TRUE(eventPool != nullptr);
@@ -59,3 +61,111 @@ static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest, TestSize.Level3)
     std::shared_ptr<ActiveKeyEvent> activeKeyEvent = std::make_shared<ActiveKeyEvent>();
     activeKeyEvent->Init(eventPool, logStoreEx);
 }
+
+/**
+ * @tc.name: ActiveKeyEventTest_002
+ * @tc.desc: ActiveKeyEventTest Init
+ * @tc.type: FUNC
+ */
+static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest_002, TestSize.Level3)
+{
+    std::shared_ptr<EventThreadPool> eventPool = nullptr;
+    EXPECT_TRUE(eventPool == nullptr);
+    std::string logStorePath = "/data/log/test/";
+    std::shared_ptr<LogStoreEx> logStoreEx = std::make_shared<LogStoreEx>(logStorePath, true);
+    auto ret = logStoreEx->Init();
+    EXPECT_EQ(ret, true);
+    std::shared_ptr<ActiveKeyEvent> activeKeyEvent = std::make_shared<ActiveKeyEvent>();
+    activeKeyEvent->Init(eventPool, logStoreEx);
+}
+
+/**
+ * @tc.name: ActiveKeyEventTest_003
+ * @tc.desc: ActiveKeyEventTest InitSubscribe
+ * @tc.type: FUNC
+ */
+static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest_003, TestSize.Level3)
+{
+    std::shared_ptr<ActiveKeyEvent> activeKeyEvent = std::make_shared<ActiveKeyEvent>();
+    activeKeyEvent->InitSubscribe(OHOS::MMI::KeyEvent::KEYCODE_VOLUME_UP, OHOS::MMI::KeyEvent::KEYCODE_VOLUME_DOWN, 5);
+}
+
+/**
+ * @tc.name: ActiveKeyEventTest_004
+ * @tc.desc: ActiveKeyEventTest CombinationKeyCallback
+ * @tc.type: FUNC
+ */
+static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest_004, TestSize.Level3)
+{
+    std::shared_ptr<ActiveKeyEvent> activeKeyEvent = std::make_shared<ActiveKeyEvent>();
+    activeKeyEvent->triggeringTime_ = (uint64_t)ActiveKeyEvent::SystemTimeMillisecond();
+    std::shared_ptr<OHOS::MMI::KeyEvent> keyEvent = OHOS::MMI::KeyEvent::Create();
+    activeKeyEvent->CombinationKeyCallback(keyEvent);
+}
+
+/**
+ * @tc.name: ActiveKeyEventTest_005
+ * @tc.desc: ActiveKeyEventTest CombinationKeyCallback
+ * @tc.type: FUNC
+ */
+static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest_005, TestSize.Level3)
+{
+    std::shared_ptr<EventThreadPool> eventPool = std::make_shared<EventThreadPool>(5, "EventThreadPool");
+    std::shared_ptr<ActiveKeyEvent> activeKeyEvent = std::make_shared<ActiveKeyEvent>();
+    activeKeyEvent->eventPool_ = eventPool;
+    activeKeyEvent->triggeringTime_ = 0;
+    auto keyEvent = OHOS::MMI::KeyEvent::Create();
+    activeKeyEvent->CombinationKeyCallback(keyEvent);
+}
+
+/**
+ * @tc.name: ActiveKeyEventTest_006
+ * @tc.desc: ActiveKeyEventTest CombinationKeyCallback
+ * @tc.type: FUNC
+ */
+static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest_006, TestSize.Level3)
+{
+    std::shared_ptr<EventThreadPool> eventPool = nullptr;
+    std::shared_ptr<ActiveKeyEvent> activeKeyEvent = std::make_shared<ActiveKeyEvent>();
+    activeKeyEvent->eventPool_ = eventPool;
+    activeKeyEvent->triggeringTime_ = 0;
+    auto keyEvent = OHOS::MMI::KeyEvent::Create();
+    activeKeyEvent->CombinationKeyCallback(keyEvent);
+}
+
+/**
+ * @tc.name: ActiveKeyEventTest_007
+ * @tc.desc: ActiveKeyEventTest CombinationKeyCallback
+ * @tc.type: FUNC
+ */
+static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest_007, TestSize.Level3)
+{
+    std::shared_ptr<EventThreadPool> eventPool = std::make_shared<EventThreadPool>(5, "EventThreadPool");
+    std::string logStorePath = "/data/log/test/";
+    std::shared_ptr<LogStoreEx> logStoreEx = std::make_shared<LogStoreEx>(logStorePath, true);
+    logStoreEx->Init();
+    std::shared_ptr<ActiveKeyEvent> activeKeyEvent = std::make_shared<ActiveKeyEvent>();
+    activeKeyEvent->Init(eventPool, logStoreEx);
+    activeKeyEvent->triggeringTime_ = 0;
+    auto keyEvent = OHOS::MMI::KeyEvent::Create();
+    activeKeyEvent->CombinationKeyCallback(keyEvent);
+}
+
+/**
+ * @tc.name: ActiveKeyEventTest_008
+ * @tc.desc: ActiveKeyEventTest CombinationKeyHandle
+ * @tc.type: FUNC
+ */
+static HWTEST_F(ActiveKeyEventTest, ActiveKeyEventTest_008, TestSize.Level3)
+{
+    std::shared_ptr<EventThreadPool> eventPool = nullptr;
+    std::string logStorePath = "/data/log/test/";
+    std::shared_ptr<LogStoreEx> logStoreEx = std::make_shared<LogStoreEx>(logStorePath, true);
+    logStoreEx->Init();
+    std::shared_ptr<ActiveKeyEvent> activeKeyEvent = std::make_shared<ActiveKeyEvent>();
+    activeKeyEvent->Init(eventPool, logStoreEx);
+    activeKeyEvent->triggeringTime_ = 0;
+    auto keyEvent = OHOS::MMI::KeyEvent::Create();
+    activeKeyEvent->CombinationKeyHandle(keyEvent);
+}
+
