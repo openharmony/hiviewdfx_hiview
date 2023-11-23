0486bd599b570f288c0ffefc29ee62ead2dd343d
zyxzyx <zhangyixin19@huawei.com>
2023-11-23 16:56:22 +0800

    add mem profiler collector
    Signed-off-by: zyxzyx <zhangyixin19@huawei.com>

---
+++ framework/native/unified_collection/collector/mem_profiler_collector_impl.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include <thread>
+#include <common.h>
+#include "logger.h"
+#include "mem_profiler_collector.h"
+#include "native_memory_profiler_sa_client_manager.h"
+#include "native_memory_profiler_sa_config.h"
+#include "parameters.h"
+
+using namespace OHOS::Developtools::NativeDaemon;
+
+namespace OHOS {
+namespace HiviewDFX {
+namespace UCollectUtil {
+DEFINE_LOG_TAG("UCollectUtil-MemProfilerCollector");
+
+const std::string NATIVE_DAEMON_NAME("native_daemon");
+int g_nativeDaemonPid = 0;
+constexpr int waitExitMills = 1000;
+
+class MemProfilerCollectorImpl : public MemProfilerCollector {
+public:
+    MemProfilerCollectorImpl() = default;
+    virtual ~MemProfilerCollectorImpl() = default;
+
+public:
+    virtual void start(Developtools::NativeDaemon::NativeMemoryProfilerSaClientManager::NativeMemProfilerType type,
+                       int pid, int duration, int sampleInterval) override;
+    virtual void stop(int pid) override;
+};
+
+void MemProfilerCollectorImpl::start(Developtools::NativeDaemon::NativeMemoryProfilerSaClientManager::NativeMemProfilerType type,
+           int pid, int duration, int sampleInterval)
+{
+    OHOS::system::SetParameter("hiviewdfx.hiprofiler.memprofiler.start", "1");
+    while (!COMMON::IsProcessExist(NATIVE_DAEMON_NAME, g_nativeDaemonPid)) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(waitExitMills));
+    }
+    NativeMemoryProfilerSaClientManager::Start(type, pid, duration, sampleInterval);
+}
+
+void MemProfilerCollectorImpl::stop(int pid)
+{
+    OHOS::system::SetParameter("hiviewdfx.hiprofiler.memprofiler.start", "0");
+    while (COMMON::IsProcessExist(NATIVE_DAEMON_NAME, g_nativeDaemonPid)) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(waitExitMills));
+    }
+    NativeMemoryProfilerSaClientManager::Stop(pid);
+}
+
+std::shared_ptr<MemProfilerCollector> MemProfilerCollector::Create()
+{
+    static std::shared_ptr<MemProfilerCollector> instance_ = std::make_shared<MemProfilerCollectorImpl>();
+    return instance_;
+}
+
+} // UCollectUtil
+} // HiViewDFX
+} // OHOS
---
+++ interfaces/inner_api/unified_collection/utility/mem_profiler_collector.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef INTERFACES_INNER_API_UNIFIED_COLLECTION_UTILITY_MEM_PROFILER_COLLECTOR_H
+#define INTERFACES_INNER_API_UNIFIED_COLLECTION_UTILITY_MEM_PROFILER_COLLECTOR_H
+#include <memory>
+#include "collect_result.h"
+#include "native_memory_profiler_sa_client_manager.h"
+
+namespace OHOS {
+namespace HiviewDFX {
+namespace UCollectUtil {
+class MemProfilerCollector {
+public:
+    MemProfilerCollector() = default;
+    virtual ~MemProfilerCollector() = default;
+
+public:
+    virtual void start(Developtools::NativeDaemon::NativeMemoryProfilerSaClientManager::NativeMemProfilerType type,
+                       int pid, int duration, int sampleInterval) = 0;
+    virtual void stop(int pid = 0);
+    static std::shared_ptr<MemProfilerCollector> Create();
+}; // MemProfilerCollector
+} // UCollectUtil
+} // HiviewDFX
+} // OHOS
+#endif // INTERFACES_INNER_API_UNIFIED_COLLECTION_UTILITY_MEM_PROFILER_COLLECTOR_H
