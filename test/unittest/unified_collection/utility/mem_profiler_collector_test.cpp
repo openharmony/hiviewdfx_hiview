b2200fcbd6f83ee6a5a6aacc0392e8d6afb85c97
zyxzyx <zhangyixin19@huawei.com>
2023-11-28 13:16:12 +0800

    fix test
    Signed-off-by: zyxzyx <zhangyixin19@huawei.com>

--- test/unittest/unified_collection/utility/mem_profiler_collector_test.cpp
+++ test/unittest/unified_collection/utility/mem_profiler_collector_test.cpp
@@ -48,10 +48,10 @@ public:
 HWTEST_F(MemProfilerCollectorTest, MemProfilerCollectorTest001, TestSize.Level1)
 {
     std::shared_ptr<MemProfilerCollector> collector = MemProfilerCollector::Create();
-    collector->start(NativeMemoryProfilerSaClientManager::NativeMemProfilerType::MEM_PROFILER_LIBRARY,
+    collector->Start(NativeMemoryProfilerSaClientManager::NativeMemProfilerType::MEM_PROFILER_LIBRARY,
                      0, DURATION, INTERVAL);
     int time = 0;
-    while(!COMMON::IsProcessExit(NATIVE_DAEMON_NAME, g_nativeDaemonPid) && time < FINAL_TIME) {
+    while(!COMMON::IsProcessExist(NATIVE_DAEMON_NAME, g_nativeDaemonPid) && time < FINAL_TIME) {
         std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_EXIT_MILLS));
         time += WAIT_EXIT_MILLS;
     }
@@ -66,12 +66,12 @@ HWTEST_F(MemProfilerCollectorTest, MemProfilerCollectorTest001, TestSize.Level1)
 HWTEST_F(MemProfilerCollectorTest, MemProfilerCollectorTest002, TestSize.Level1)
 {
     std::shared_ptr<MemProfilerCollector> collector = MemProfilerCollector::Create();
-    collector->start(NativeMemoryProfilerSaClientManager::NativeMemProfilerType::MEM_PROFILER_LIBRARY,
+    collector->Start(NativeMemoryProfilerSaClientManager::NativeMemProfilerType::MEM_PROFILER_LIBRARY,
                      0, DURATION, INTERVAL);
     std::this_thread::sleep_for(std::chrono::milliseconds(FINAL_TIME));
-    collector->stop(0);
+    collector->Stop(0);
     int time = 0;
-    while(COMMON::IsProcessExit(NATIVE_DAEMON_NAME, g_nativeDaemonPid) && time < FINAL_TIME) {
+    while(COMMON::IsProcessExist(NATIVE_DAEMON_NAME, g_nativeDaemonPid) && time < FINAL_TIME) {
         std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_EXIT_MILLS));
         time += WAIT_EXIT_MILLS;
     }
