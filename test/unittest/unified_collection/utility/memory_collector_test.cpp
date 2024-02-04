14c6e0e1c2e29caf04c24a839266fc9a8d87ea7e
wenlong_12 <wenlong12@huawei.com>
2024-02-04 08:47:54 +0800

    modified tests to check format
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

--- test/unittest/unified_collection/utility/memory_collector_test.cpp
+++ test/unittest/unified_collection/utility/memory_collector_test.cpp
@@ -14,7 +14,10 @@
  */
 #include <dlfcn.h>
 #include <fcntl.h>
+#include <fstream>
 #include <iostream>
+#include <regex>
+#include <string>
 
 #include "memory_collector.h"
 
@@ -39,6 +42,27 @@ bool HasValidAILibrary()
     const std::string libName = "libai_infra.so";
     void* handle = dlopen(libName.c_str(), RTLD_LAZY);
     return handle != nullptr;
+}
+
+bool CheckFormat(const std::string &fileName, std::regex &reg1, std::regex &reg2, std::regex &reg3, int cnt)
+{
+    std::ifstream file;
+    file.open(fileName.c_str());
+    if (!file.is_open()) {
+        return false;
+    }
+    std::string line;
+    while (cnt--) {
+        getline(file, line);
+    }
+    while (getline(file, line)) {
+        if (!((regex_match(line, reg1) && regex_match(line, reg2)) || regex_match(line, reg3))) {
+            file.close();
+            return false;
+        }
+    }
+    file.close();
+    return true;
 }
 }
 
@@ -79,6 +103,12 @@ HWTEST_F(MemoryCollectorTest, MemoryCollectorTest003, TestSize.Level1)
     CollectResult<std::string> data = collector->CollectRawMemInfo();
     std::cout << "collect raw memory info result" << data.retCode << std::endl;
     ASSERT_TRUE(data.retCode == UcError::SUCCESS);
+
+    std::regex reg1("^[\\w()]{1,}:\\s{1,}\\d{1,}$");
+    std::regex reg2("^[\\w\\s:()]{24,}$");
+    std::regex reg3("^[\\w()]{1,}:\\s{1,}\\d{1,} kB$");
+    bool flag = CheckFormat(data.data, reg1, reg2, reg3, 0);    //0: don't skip the first line
+    ASSERT_TRUE(flag);
 }
 
 /**
@@ -105,6 +135,12 @@ HWTEST_F(MemoryCollectorTest, MemoryCollectorTest005, TestSize.Level1)
     CollectResult<std::string> data = collector->ExportAllProcessMemory();
     std::cout << "export all process memory result" << data.retCode << std::endl;
     ASSERT_TRUE(data.retCode == UcError::SUCCESS);
+
+    std::regex reg1("^\\d{1,}\\s{1,}[\\w\\.:-]{1,}(\\s{1,}\\d{1,}){3}\\s{1,}[\\d-]{1,}$");
+    std::regex reg2("^[\\w\\s\\.:-]{1,}$");
+    std::regex reg3("^used to take a place$");
+    bool flag = CheckFormat(data.data, reg1, reg2, reg3, 1);    //1: skip the first line
+    ASSERT_TRUE(flag);
 }
 
 /**
@@ -118,6 +154,15 @@ HWTEST_F(MemoryCollectorTest, MemoryCollectorTest006, TestSize.Level1)
     CollectResult<std::string> data = collector->CollectRawSlabInfo();
     std::cout << "collect raw slab info result" << data.retCode << std::endl;
     ASSERT_TRUE(data.retCode == UcError::SUCCESS);
+
+    std::string first = "^[\\w\\[\\]-]{1,}(\\s{1,}\\d{1,}){5}( : tunables)(\\s{1,}\\d{1,}){3}";
+    std::string second = "( : slabdata)(\\s{1,}\\d{1,}){3}$";
+    std::string regStr = first + second;
+    std::regex reg1(regStr);
+    std::regex reg2("^[\\w\\s:\\[\\]-]{1,}$");
+    std::regex reg3("^(\\w{1,} - version: )[\\d\\.]{1,}|(#name)[\\s\\w<>:]{1,}$");
+    bool flag = CheckFormat(data.data, reg1, reg2, reg3, 0);    //0: don't skip the first line
+    ASSERT_TRUE(flag);
 }
 
 /**
@@ -131,6 +176,12 @@ HWTEST_F(MemoryCollectorTest, MemoryCollectorTest007, TestSize.Level1)
     CollectResult<std::string> data = collector->CollectRawPageTypeInfo();
     std::cout << "collect raw pagetype info result" << data.retCode << std::endl;
     ASSERT_TRUE(data.retCode == UcError::SUCCESS);
+
+    std::regex reg1("^(Node)\\s{1,}\\d{1,}(, zone)\\s{1,}\\w{1,}(, type)\\s{1,}\\w{1,}(\\s{1,}\\d{1,}){21} $");
+    std::regex reg2("^(Node)[\\s\\d]{5,}(, zone)[\\s\\w]{9,}(, type)[\\s\\w]{13}([\\s\\d]{7}){21} $");
+    std::regex reg3("^used to take a place$");
+    bool flag = CheckFormat(data.data, reg1, reg2, reg3, 4);    //4: skip the first four lines
+    ASSERT_TRUE(flag);
 }
 
 /**
@@ -144,6 +195,12 @@ HWTEST_F(MemoryCollectorTest, MemoryCollectorTest008, TestSize.Level1)
     CollectResult<std::string> data = collector->CollectRawDMA();
     std::cout << "collect raw DMA result" << data.retCode << std::endl;
     ASSERT_TRUE(data.retCode == UcError::SUCCESS);
+
+    std::regex reg1("^[\\w:\\.]{1,}(\\s{1,}\\d{1,}){5}(\\s{1,}\\w{1,}){3}$");
+    std::regex reg2("^[\\w:\\.\\s]{17}([\\d\\s]{17}){5}[\\s\\w]{1,}$");
+    std::regex reg3("^(Total dmabuf size of )[\\w:]{1,}(: )\\d{1,}( bytes)$");
+    bool flag = CheckFormat(data.data, reg1, reg2, reg3, 2);    //2: skip the first two lines
+    ASSERT_TRUE(flag);
 }
 
 /**
