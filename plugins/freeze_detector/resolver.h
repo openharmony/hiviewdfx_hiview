25c5ec3c9017b4637ee36eea4016890e9c3249d0
hhl <yinhuiling@huawei.com>
2024-02-20 20:12:30 +0800

    fix codecheck
    Signed-off-by: hhl <yinhuiling@huawei.com>

--- plugins/freeze_detector/resolver.h
+++ plugins/freeze_detector/resolver.h
@@ -47,6 +47,7 @@ private:
     static const int DEFAULT_TIME_WINDOW = 30;
     static const int MINUTES_IN_HOUR = 60;
     static const int minMatchNum = 2;
+    static const int defaultHours = 10;
 
     bool ResolveEvent(const WatchPoint& watchPoint,
     std::vector<WatchPoint>& list, std::vector<FreezeResult>& result) const;
