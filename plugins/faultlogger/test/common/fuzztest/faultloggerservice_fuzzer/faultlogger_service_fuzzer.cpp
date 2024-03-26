b675fb8bd1419aca35d45c3c351134c5dc6f1f3c
p30054102 <panlinzhi@huawei.com>
2024-03-26 15:25:29 +0800

    提高faultlog模块fuzz覆盖率
    Signed-off-by: p30054102 <panlinzhi@huawei.com>

--- plugins/faultlogger/test/common/fuzztest/faultloggerservice_fuzzer/faultlogger_service_fuzzer.cpp
+++ plugins/faultlogger/test/common/fuzztest/faultloggerservice_fuzzer/faultlogger_service_fuzzer.cpp
@@ -18,16 +18,19 @@
 #include <string>
 #include <vector>
 #include "faultlogger.h"
+#include "faultlog_manager.h"
 #include "faultlogger_service_ohos.h"
 #include "faultlogger_service_fuzzer.h"
 #include "hiview_platform.h"
 #include "securec.h"
-namespace OHOS {
 
-std::shared_ptr<HiviewDFX::Faultlogger> CreateFaultloggerInstance()
+using namespace OHOS::HiviewDFX;
+namespace OHOS {
+const int32_t FAULTLOGTYPE_SIZE = 6;
+std::shared_ptr<Faultlogger> CreateFaultloggerInstance()
 {
-    static std::unique_ptr<HiviewDFX::HiviewPlatform> platform = std::make_unique<HiviewDFX::HiviewPlatform>();
-    auto plugin = std::make_shared<HiviewDFX::Faultlogger>();
+    static std::unique_ptr<HiviewPlatform> platform = std::make_unique<HiviewPlatform>();
+    auto plugin = std::make_shared<Faultlogger>();
     plugin->SetName("Faultlogger");
     plugin->SetHandle(nullptr);
     plugin->SetHiviewContext(platform.get());
@@ -38,10 +41,11 @@ std::shared_ptr<HiviewDFX::Faultlogger> CreateFaultloggerInstance()
 void FuzzServiceInterfaceDump(const uint8_t* data, size_t size)
 {
     auto service = CreateFaultloggerInstance();
-    HiviewDFX::FaultloggerServiceOhos serviceOhos;
-    HiviewDFX::FaultloggerServiceOhos::StartService(service.get());
-    if (HiviewDFX::FaultloggerServiceOhos::GetOrSetFaultlogger(nullptr) != service.get()) {
+    FaultloggerServiceOhos serviceOhos;
+    FaultloggerServiceOhos::StartService(service.get());
+    if (FaultloggerServiceOhos::GetOrSetFaultlogger(nullptr) != service.get()) {
         printf("FaultloggerServiceOhos start service error.\n");
+        return;
     }
 
     int32_t fd = static_cast<int32_t>(*data);
@@ -61,20 +65,99 @@ void FuzzServiceInterfaceDump(const uint8_t* data, size_t size)
 void FuzzServiceInterfaceQuerySelfFaultLog(const uint8_t* data, size_t size)
 {
     auto service = CreateFaultloggerInstance();
-    HiviewDFX::FaultloggerServiceOhos serviceOhos;
-    HiviewDFX::FaultloggerServiceOhos::StartService(service.get());
-    if (HiviewDFX::FaultloggerServiceOhos::GetOrSetFaultlogger(nullptr) != service.get()) {
+    FaultloggerServiceOhos serviceOhos;
+    FaultloggerServiceOhos::StartService(service.get());
+    if (FaultloggerServiceOhos::GetOrSetFaultlogger(nullptr) != service.get()) {
         printf("FaultloggerServiceOhos start service error.\n");
+        return;
     }
     int32_t faultType = static_cast<int32_t>(*data);
     int32_t maxNum = static_cast<int32_t>(*data);
-    (void)serviceOhos.QuerySelfFaultLog(faultType, maxNum);
+    auto remoteObject = serviceOhos.QuerySelfFaultLog(faultType, maxNum);
+    auto result = iface_cast<FaultLogQueryResultOhos>(remoteObject);
+    if (result != nullptr) {
+        while (result->HasNext()) {
+            result->GetNext();
+        }
+    }
+}
+
+void FuzzServiceInterfaceCreateTempFaultLogFile(const uint8_t* data, size_t size)
+{
+    auto faultLogManager = std::make_unique<FaultLogManager>(nullptr);
+    faultLogManager->Init();
+
+    int64_t time = static_cast<int64_t>(*data);
+    int32_t id = static_cast<int32_t>(*data);
+    int32_t faultType = static_cast<int32_t>(*data);
+    std::string module = std::string(reinterpret_cast<const char*>(data), size);
+    faultLogManager->CreateTempFaultLogFile(time, id, faultType, module);
+}
+
+void FuzzServiceInterfaceAddFaultLog(const uint8_t* data, size_t size)
+{
+    auto service = CreateFaultloggerInstance();
+    FaultloggerServiceOhos serviceOhos;
+    FaultloggerServiceOhos::StartService(service.get());
+    if (FaultloggerServiceOhos::GetOrSetFaultlogger(nullptr) != service.get()) {
+        printf("FaultloggerServiceOhos start service error.\n");
+        return;
+    }
+    FaultLogInfoOhos info;
+    info.time = static_cast<int64_t>(*data);
+    info.pid = static_cast<int32_t>(*data);
+    info.uid = static_cast<int32_t>(*data);
+    info.faultLogType = static_cast<int32_t>(*data) % FAULTLOGTYPE_SIZE;
+    info.module = std::string(reinterpret_cast<const char*>(data), size);
+    info.logPath = std::string(reinterpret_cast<const char*>(data), size);
+    info.reason = std::string(reinterpret_cast<const char*>(data), size);
+    info.registers = std::string(reinterpret_cast<const char*>(data), size);
+    info.sectionMaps["HILOG"] = std::string(reinterpret_cast<const char*>(data), size);
+    info.sectionMaps["KEYLOGFILE"] = std::string(reinterpret_cast<const char*>(data), size);
+    serviceOhos.AddFaultLog(info);
+    serviceOhos.Destroy();
+}
+
+void FuzzServiceInterfaceGetFaultLogInfo(const uint8_t* data, size_t size)
+{
+    auto service = CreateFaultloggerInstance();
+    std::vector<std::string> files;
+    FileUtil::GetDirFiles("/data/log/faultlog/temp/", files);
+    for (const auto& file : files) {
+        service->GetFaultLogInfo(file);
+    }
+}
+
+void FuzzServiceInterfaceOnEvent(const uint8_t* data, size_t size)
+{
+    auto service = CreateFaultloggerInstance();
+    std::string domain = std::string(reinterpret_cast<const char*>(data), size);
+    std::string eventName = std::string(reinterpret_cast<const char*>(data), size);
+    SysEventCreator sysEventCreator(domain, eventName, SysEventCreator::FAULT);
+    std::map<std::string, std::string> bundle;
+    bundle["HILOG"] = std::string(reinterpret_cast<const char*>(data), size);
+    bundle["KEYLOGFILE"] = std::string(reinterpret_cast<const char*>(data), size);
+    sysEventCreator.SetKeyValue("name_", "JS_ERROR");
+    sysEventCreator.SetKeyValue("pid_", static_cast<int32_t>(*data));
+    sysEventCreator.SetKeyValue("uid_", static_cast<int32_t>(*data));
+    sysEventCreator.SetKeyValue("tid_", static_cast<int32_t>(*data));
+    sysEventCreator.SetKeyValue("SUMMARY", std::string(reinterpret_cast<const char*>(data), size));
+    sysEventCreator.SetKeyValue("PACKAGE_NAME", std::string(reinterpret_cast<const char*>(data), size));
+    sysEventCreator.SetKeyValue("bundle_", bundle);
+    std::string desc = std::string(reinterpret_cast<const char*>(data), size);
+    auto sysEvent = std::make_shared<SysEvent>(desc, nullptr, sysEventCreator);
+    auto event = std::dynamic_pointer_cast<Event>(sysEvent);
+    service->OnEvent(event);
 }
 
 void FuzzFaultloggerServiceInterface(const uint8_t* data, size_t size)
 {
     FuzzServiceInterfaceDump(data, size);
     FuzzServiceInterfaceQuerySelfFaultLog(data, size);
+    FuzzServiceInterfaceCreateTempFaultLogFile(data, size);
+    FuzzServiceInterfaceAddFaultLog(data, size);
+    FuzzServiceInterfaceGetFaultLogInfo(data, size);
+    FuzzServiceInterfaceOnEvent(data, size);
 }
 }
 
