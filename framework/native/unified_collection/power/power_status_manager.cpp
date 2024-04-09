ae06b88639ea8e32614e1940d1d5fb269ddf2b63
杨世琦_hw <670097973@qq.com>
2024-04-09 15:12:38 +0800

    add power state status to process state
    Signed-off-by: 杨世琦_hw <670097973@qq.com>

---
+++ framework/native/unified_collection/power/power_status_manager.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "power_status_manager.h"
+
+#include <common_event_support.h>
+#include <common_event_manager.h>
+#include "logger.h"
+#include "power_mgr_client.h"
+
+namespace OHOS {
+namespace HiviewDFX {
+namespace UCollectUtil {
+DEFINE_LOG_TAG("UCollectUtil-PowerState");
+void PowerStateSubscriber::OnReceiveEvent(const CommonEventData &data)
+{
+    std::string action = data.GetWant().GetAction();
+    HIVIEW_LOGD("OnReceiveEvent action%{public}s", action.c_str());
+    if (action == CommonEventSupport::COMMON_EVENT_SCREEN_ON) {
+        PowerStatusManager::GetInstance().SetPowerState(SCREEN_ON);
+    } else if (action == CommonEventSupport::COMMON_EVENT_SCREEN_OFF) {
+        PowerStatusManager::GetInstance().SetPowerState(SCREEN_OFF);
+    } else {
+    }
+}
+
+PowerStatusManager::PowerStatusManager()
+{
+    powerState_ = PowerMgr::PowerMgrClient::GetInstance().IsScreenOn() ? SCREEN_ON : SCREEN_OFF;
+    MatchingSkills matchingSkills;
+    matchingSkills.AddEvent(CommonEventSupport::COMMON_EVENT_SCREEN_ON);
+    matchingSkills.AddEvent(CommonEventSupport::COMMON_EVENT_SCREEN_OFF);
+    CommonEventSubscribeInfo info(matchingSkills);
+    powerStateSubscriber = std::make_shared<PowerStateSubscriber>(info);
+    if (!CommonEventManager::SubscribeCommonEvent(powerStateSubscriber)) {
+        HIVIEW_LOGE("register PowerStateSubscriber fail");
+    }
+}
+
+PowerStatusManager::~PowerStatusManager()
+{
+    if (powerStateSubscriber != nullptr) {
+        CommonEventManager::UnSubscribeCommonEvent(powerStateSubscriber);
+    }
+}
+
+void PowerStatusManager::SetPowerState(PowerState powerState)
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    powerState_ = powerState;
+}
+
+int32_t PowerStatusManager::GetPowerState()
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    return powerState_;
+}
+}
+}
+}
