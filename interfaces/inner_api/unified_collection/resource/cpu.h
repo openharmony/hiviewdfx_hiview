fa6e290507a76419b75844f325592b79e4940699
杨世琦_hw <670097973@qq.com>
2024-04-11 12:10:57 +0000

    update interfaces/inner_api/unified_collection/resource/cpu.h.
    Signed-off-by: 杨世琦_hw <670097973@qq.com>

--- interfaces/inner_api/unified_collection/resource/cpu.h
+++ interfaces/inner_api/unified_collection/resource/cpu.h
@@ -61,7 +61,7 @@ struct ProcessCpuStatInfo {
     double sCpuUsage = 0;
     double cpuUsage = 0;
     std::string procName;
-    int32_t threadCount;
+    int32_t threadCount = 0;
 };
 
 struct ThreadCpuStatInfo {
