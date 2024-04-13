f74e040957e3f84cb92eeea97d5b7f26b819c38f
lyj_love_code <liangyujian2@huawei.com>
2024-04-13 14:34:30 +0800

    fix: fix the parsing logic for db versions
    Signed-off-by: lyj_love_code <liangyujian2@huawei.com>

--- base/event_store/test/unittest/common/sys_event_store_utility_test.cpp
+++ base/event_store/test/unittest/common/sys_event_store_utility_test.cpp
@@ -28,10 +28,15 @@
 
 namespace OHOS {
 namespace HiviewDFX {
-DEFINE_LOG_TAG("ContentReaderVersionTest");
+DEFINE_LOG_TAG("SysEventStoreUtilityTest");
+using namespace  OHOS::HiviewDFX::EventStore;
 namespace {
 constexpr int8_t TEST_READER_VERSION = 100;
 constexpr uint32_t TEST_EVENT_SIZE = 200;
+constexpr int32_t TEST_EVENT_TYPE = 4;
+const std::string TEST_DB_VERSION1_FILE = "/data/test/TEST_DOMAIN/TEST_VERSION1-1-CRITICAL-1.db";
+const std::string TEST_DB_VERSION2_FILE = "/data/test/TEST_DOMAIN/TEST_VERSION2-1-CRITICAL-1.db";
+const std::string TEST_DB_VERSION3_FILE = "/data/test/TEST_DOMAIN/TEST_VERSION3-1-CRITICAL-1.db";
 class ContentReaderVersionTest : public ContentReader {
 public:
     int ReadDocDetails(std::ifstream& docStream, EventStore::DocHeader& header,
@@ -66,6 +71,89 @@ protected:
         return sizeof(EventStore::ContentHeader);
     }
 };
+
+bool CompareSeqFuncGreater(const Entry& entryA, const Entry& entryB)
+{
+    return entryA.id > entryB.id;
+}
+
+void TestFileSizeOfDocReader(const std::string& path)
+{
+    SysEventDocReader reader(path);
+    ASSERT_GT(reader.ReadFileSize(), 0);
+}
+
+void TestPageSizeOfDocReader(const std::string& path)
+{
+    SysEventDocReader reader(path);
+    uint32_t pageSize = 0;
+    ASSERT_EQ(reader.ReadPageSize(pageSize), DOC_STORE_SUCCESS);
+    ASSERT_GT(pageSize, 0);
+}
+
+void TestHeaderOfDocReader(const std::string& path)
+{
+    SysEventDocReader reader(path);
+    DocHeader header;
+    ASSERT_EQ(reader.ReadHeader(header), DOC_STORE_SUCCESS);
+    ASSERT_GT(header.magicNum, 0);
+    ASSERT_GT(header.blockSize, 0);
+    ASSERT_GT(header.pageSize, 0);
+    ASSERT_GT(header.version, 0);
+}
+
+void TestSystemVersionOfDocReader(const std::string& path, bool isVersionEmpty = false)
+{
+    SysEventDocReader reader(path);
+    DocHeader header;
+    std::string sysVersion;
+    ASSERT_EQ(reader.ReadHeader(header, sysVersion), DOC_STORE_SUCCESS);
+    ASSERT_EQ(sysVersion.empty(), isVersionEmpty);
+}
+
+void CheckEvent(SysEvent& event)
+{
+    ASSERT_TRUE(event.domain_ == "TEST_DOMAIN");
+    ASSERT_TRUE(event.eventName_.find("TEST_VERSION") == 0);
+    ASSERT_EQ(event.eventType_, TEST_EVENT_TYPE);
+    ASSERT_GT(event.happenTime_, 0);
+    ASSERT_GE(event.GetTz(), 0);
+    ASSERT_GE(event.GetPid(), 0);
+    ASSERT_GE(event.GetTid(), 0);
+    ASSERT_GE(event.GetUid(), 0);
+    ASSERT_GE(event.GetSeq(), 0);
+
+    std::string eventJsonStr = event.AsJsonStr();
+    HIVIEW_LOGI("event=%{public}s", eventJsonStr.c_str());
+    ASSERT_FALSE(eventJsonStr.empty());
+    ASSERT_TRUE(eventJsonStr.find(EventCol::DOMAIN) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::NAME) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::TYPE) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::TS) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::TZ) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::PID) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::TID) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::UID) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::LEVEL) != std::string::npos);
+    ASSERT_TRUE(eventJsonStr.find(EventCol::SEQ) != std::string::npos);
+}
+
+void TestEventsOfDocReader(const std::string& path, bool isVersionEmpty = false)
+{
+    SysEventDocReader reader(path);
+    DocQuery query;
+    EntryQueue entries(CompareSeqFuncGreater);
+    int num = 0;
+    ASSERT_EQ(reader.Read(query, entries, num), DOC_STORE_SUCCESS);
+    ASSERT_GT(num, 0);
+
+    while (!entries.empty()) {
+        auto& entry = entries.top();
+        SysEvent event("", nullptr, entry.data, entry.id);
+        CheckEvent(event);
+        entries.pop();
+    }
+}
 }
 
 void SysEventStoreUtilityTest::SetUpTestCase()
@@ -109,7 +197,7 @@ HWTEST_F(SysEventStoreUtilityTest, SysEventStoreUtilityTest001, testing::ext::Te
 HWTEST_F(SysEventStoreUtilityTest, SysEventStoreUtilityTest002, testing::ext::TestSize.Level3)
 {
     std::ifstream dbFileStream;
-    dbFileStream.open("/data/test/test_data/version1_db_file", std::ios::binary);
+    dbFileStream.open(TEST_DB_VERSION1_FILE, std::ios::binary);
     ASSERT_TRUE(dbFileStream.is_open());
     auto version = ContentReader::ReadFmtVersion(dbFileStream);
     ASSERT_EQ(version, EventStore::EVENT_DATA_FORMATE_VERSION::VERSION1);
@@ -123,7 +211,7 @@ HWTEST_F(SysEventStoreUtilityTest, SysEventStoreUtilityTest002, testing::ext::Te
     ASSERT_EQ(header.version, EventStore::EVENT_DATA_FORMATE_VERSION::VERSION1);
     ASSERT_EQ(sysVersion.size(), 0);
     dbFileStream.close();
-    dbFileStream.open("/data/test/test_data/version2_db_file", std::ios::binary);
+    dbFileStream.open(TEST_DB_VERSION2_FILE, std::ios::binary);
     ASSERT_TRUE(dbFileStream.is_open());
     version = ContentReader::ReadFmtVersion(dbFileStream);
     ASSERT_EQ(version, EventStore::EVENT_DATA_FORMATE_VERSION::VERSION2);
@@ -145,7 +233,7 @@ HWTEST_F(SysEventStoreUtilityTest, SysEventStoreUtilityTest002, testing::ext::Te
 HWTEST_F(SysEventStoreUtilityTest, SysEventStoreUtilityTest003, testing::ext::TestSize.Level3)
 {
     std::ifstream dbFileStream;
-    dbFileStream.open("/data/test/test_data/version3_db_file", std::ios::binary);
+    dbFileStream.open(TEST_DB_VERSION3_FILE, std::ios::binary);
     auto version = ContentReader::ReadFmtVersion(dbFileStream);
     ASSERT_EQ(version, EventStore::EVENT_DATA_FORMATE_VERSION::VERSION3);
     auto reader = ContentReaderFactory::GetInstance().Get(version);
@@ -165,6 +253,35 @@ HWTEST_F(SysEventStoreUtilityTest, SysEventStoreUtilityTest003, testing::ext::Te
     auto ret = reader->ReadRawData(info, content, contentSize);
     ASSERT_NE(ret, nullptr);
     dbFileStream.close();
+}
+
+/**
+ * @tc.name: SysEventStoreUtilityTest004
+ * @tc.desc: SysEventDocReader test
+ * @tc.type: FUNC
+ * @tc.require: issueI9DJP3
+ */
+HWTEST_F(SysEventStoreUtilityTest, SysEventStoreUtilityTest004, testing::ext::TestSize.Level3)
+{
+    const std::vector<std::string> dbVersionPaths = {
+        TEST_DB_VERSION1_FILE,
+        TEST_DB_VERSION2_FILE,
+        TEST_DB_VERSION3_FILE
+    };
+    for (const auto& dbPath : dbVersionPaths) {
+        TestFileSizeOfDocReader(dbPath);
+        TestPageSizeOfDocReader(dbPath);
+        TestHeaderOfDocReader(dbPath);
+
+        // version1 and version2 do not support the sys version
+        if (dbPath == TEST_DB_VERSION1_FILE || dbPath == TEST_DB_VERSION2_FILE) {
+            TestSystemVersionOfDocReader(dbPath, true);
+        } else {
+            TestSystemVersionOfDocReader(dbPath, false);
+        }
+
+        TestEventsOfDocReader(dbPath);
+    }
 }
 } // namespace HiviewDFX
 } // namespace OHOS
