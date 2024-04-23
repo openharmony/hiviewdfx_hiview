d70aa82510ee742fedf979e6eae26574df36e04a
oumingyuan <oumingyuan@huawei.com>
2024-04-23 11:41:46 +0800

    code has null pointer problem Issue:#I9IR76 Test:Build & Boot devices Signed-off-by: oumingyuan <oumingyuan@huawei.com>

--- plugins/faultlogger/service/sanitizer_collector/sanitizerd_monitor.cpp
+++ plugins/faultlogger/service/sanitizer_collector/sanitizerd_monitor.cpp
@@ -96,12 +96,12 @@ int SanitizerdMonitor::Init(SANITIZERD_NOTIFY_CALLBACK pcb)
 {
     const std::string asanLogPath = std::string(ASAN_LOG_PATH);
     gNfds = 1;
-    void *error = calloc(1, sizeof(gUfds[0]));
+    void* error = calloc(1, sizeof(gUfds[0]));
     if (!error) {
         HIVIEW_LOGI("Memory allocation failed.");
         return 1;
     }
-    gUfds = reinterpret_cast<pollfd *>(ca);
+    gUfds = reinterpret_cast<pollfd *>(error);
     gUfds[0].fd = inotify_init();
     if (gUfds[0].fd < 0) {
         HIVIEW_LOGI("inotify_init failed: %{public}d-%{public}s.", gUfds[0].fd, strerror(errno));
