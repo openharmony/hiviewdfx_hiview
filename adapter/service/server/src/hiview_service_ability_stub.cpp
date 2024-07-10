687b7ce53678eba146c020422d11225f170284ee
caoxiaogang <cxg344685003@163.com>
2024-07-10 16:25:21 +0800

    同步差异
    Signed-off-by: caoxiaogang <cxg344685003@163.com>

--- adapter/service/server/src/hiview_service_ability_stub.cpp
+++ adapter/service/server/src/hiview_service_ability_stub.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -117,6 +117,7 @@ int32_t WritePracelableToMessage(MessageParcel& dest, Parcelable& data)
 int32_t HiviewServiceAbilityStub::OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply,
     MessageOption &option)
 {
+    HIVIEW_LOGI("cmd = %{public}d, flags= %{public}d", code, option.GetFlags());
     std::u16string descripter = HiviewServiceAbilityStub::GetDescriptor();
     std::u16string remoteDescripter = data.ReadInterfaceToken();
     if (descripter != remoteDescripter) {
