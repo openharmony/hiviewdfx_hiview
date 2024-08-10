f0328b54a838495e0c695508f67f33a10801b639
lyj_love_code <liangyujian2@huawei.com>
2024-08-10 11:11:43 +0800

    bug: fix the judgment of the mallopt return value
    Signed-off-by: lyj_love_code <liangyujian2@huawei.com>

--- base/utility/include/memory_util.h
+++ base/utility/include/memory_util.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +18,9 @@
 namespace OHOS {
 namespace HiviewDFX {
 namespace MemoryUtil {
+constexpr int E_MEM_OK = 0;
+constexpr int E_MEM_ERR = -1;
+
 /**
  * Disable the cache of the current thread.
  */
