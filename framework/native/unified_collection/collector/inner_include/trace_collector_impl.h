4423a251a6b1c32a73df254b09232a3f9d4be73b
尹之帆 <yinzhifan@h-partners.com>
2024-08-13 20:22:53 +0800

    update default values
    Signed-off-by: 尹之帆 <yinzhifan@h-partners.com>

--- framework/native/unified_collection/collector/inner_include/trace_collector_impl.h
+++ framework/native/unified_collection/collector/inner_include/trace_collector_impl.h
@@ -29,7 +29,7 @@ public:
 public:
     virtual CollectResult<std::vector<std::string>> DumpTrace(UCollect::TraceCaller &caller) override;
     virtual CollectResult<std::vector<std::string>> DumpTraceWithDuration(
-        UCollect::TraceCaller &caller, uint32_t timeLimit, uint64_t happenTime) override;
+        UCollect::TraceCaller &caller, uint32_t timeLimit, uint64_t happenTime = 0) override;
     virtual CollectResult<int32_t> TraceOn() override;
     virtual CollectResult<std::vector<std::string>> TraceOff() override;
 private:
