9f98e1c86425bbff3b3c4f28894365a9946a13dc
杨世琦_hw <670097973@qq.com>
2024-08-17 15:20:57 +0800

    add ipc graphci interface
    Signed-off-by: 杨世琦_hw <670097973@qq.com>

--- adapter/service/server/src/hiview_service_ability_stub.cpp
+++ adapter/service/server/src/hiview_service_ability_stub.cpp
@@ -228,11 +228,11 @@ std::unordered_map<uint32_t, RequestHandler> HiviewServiceAbilityStub::GetMemory
 {
     static std::unordered_map<uint32_t, RequestHandler> memoryRequestHandlers = {
         {static_cast<uint32_t>(HiviewServiceInterfaceCode::HIVIEW_SERVICE_ID_SET_APPRESOURCE_LIMIT),
-             std::bind(&HiviewServiceAbilityStub::HandleSetAppResourceLimitRequest, this,
-             std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)},
+            std::bind(&HiviewServiceAbilityStub::HandleSetAppResourceLimitRequest, this,
+            std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)},
         {static_cast<uint32_t>(HiviewServiceInterfaceCode::HIVIEW_SERVICE_ID_GET_GRAPHIC_USAGE),
-             std::bind(&HiviewServiceAbilityStub::HandleGetGraphicUsageRequest, this,
-              std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)},
+            std::bind(&HiviewServiceAbilityStub::HandleGetGraphicUsageRequest, this,
+            std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)},
     };
     return memoryRequestHandlers;
 }
