1840ca9a532c5474ef2248227dc88d947203d586
郑冬冬冬 <zhengchengkai@huawei.com>
2024-08-23 11:57:20 +0800

    Message:注册监听改用ffrt执行优化:
    Signed-off-by: 郑冬冬冬 <zhengchengkai@huawei.com>

--- plugins/eventlogger/event_focus_listener.cpp
+++ plugins/eventlogger/event_focus_listener.cpp
@@ -15,18 +15,79 @@
 #ifdef WINDOW_MANAGER_ENABLE
 #include "event_focus_listener.h"
 
+#include "ffrt.h"
+#include "hiview_logger.h"
+#include "parameter_ex.h"
+#include "time_util.h"
+
 namespace OHOS {
 namespace HiviewDFX {
+DEFINE_LOG_LABEL(0xD002D01, "EventFocusListener");
 sptr<EventFocusListener> EventFocusListener::instance_ = nullptr;
 std::recursive_mutex EventFocusListener::mutex_;
+bool EventFocusListener::isRegistered_ = false;
+bool EventFocusListener::isRegistering_ = false;
+uint64_t EventFocusListener::lastChangedTime_ = TimeUtil::GetMilliseconds();
 
 sptr<EventFocusListener> EventFocusListener::GetInstance()
 {
+    std::lock_guard<std::recursive_mutex> lock_l(mutex_);
     if (instance_ == nullptr) {
-        std::lock_guard<std::recursive_mutex> lock_l(mutex_);
         instance_ = new (std::nothrow) EventFocusListener();
     }
     return instance_;
+}
+
+void EventFocusListener::RegisterFocusListener()
+{
+    if (!Parameter::IsBetaVersion()) {
+        return;
+    }
+
+    if (isRegistered_) {
+        HIVIEW_LOGD("eventFocusListener is registered");
+        return;
+    }
+    if (GetInstance() == nullptr) {
+        HIVIEW_LOGE("register eventFocusListener failed, listener is null");
+        return;
+    }
+    if (isRegistering_) {
+        HIVIEW_LOGI("register eventFocusListener task is executing");
+        return;
+    }
+
+    auto registerTask = [] {
+        isRegistering_ = true;
+        Rosen::WMError ret = Rosen::WindowManager::GetInstance().RegisterFocusChangedListener(GetInstance());
+        if (ret == Rosen::WMError::WM_OK) {
+            HIVIEW_LOGI("register eventFocusListener succeed.");
+            isRegistered_ = true;
+        }
+        isRegistering_ = false;
+    };
+    HIVIEW_LOGI("before submit registerFocusListener task to ffrt");
+    ffrt::submit(registerTask, {}, {}, ffrt::task_attr().name("rgs_fcs_lst"));
+    HIVIEW_LOGI("after submit registerFocusListener task to ffrt");
+}
+
+void EventFocusListener::UnRegisterFocusListener()
+{
+    if (!isRegistered_) {
+        HIVIEW_LOGD("eventFocusListener is unRegistered");
+        return;
+    }
+    if (GetInstance() == nullptr) {
+        HIVIEW_LOGE("unRegister eventFocusListener failed, listener is null");
+        return;
+    }
+
+    Rosen::WMError ret = Rosen::WindowManager::GetInstance().UnregisterFocusChangedListener(GetInstance());
+    if (ret == Rosen::WMError::WM_OK) {
+        HIVIEW_LOGI("unRegister eventFocusListener succeed");
+        isRegistered_ = false;
+        GetInstance() = nullptr;
+    }
 }
 
 void EventFocusListener::OnFocused(const sptr<Rosen::FocusChangeInfo>& focusChangeInfo)
