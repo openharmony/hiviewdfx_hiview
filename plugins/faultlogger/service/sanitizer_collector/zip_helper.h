5070d8007c8997fbf7ef9f0694a817b10a193fce
愚者@yj20200618 <yangjun249@huawei.com>
2024-08-30 14:20:24 +0800

    fix:delete sanitizer
    Signed-off-by: 愚者@yj20200618 <yangjun249@huawei.com>

--- plugins/faultlogger/service/sanitizer_collector/zip_helper.h
+++ plugins/faultlogger/service/sanitizer_collector/zip_helper.h
@@ -19,27 +19,16 @@
 #include <string>
 #include <vector>
 #include <sys/types.h>
-
-#include "reporter.h"
-
 namespace OHOS {
 namespace HiviewDFX {
-constexpr unsigned SL_BUF_LEN = 512;
-constexpr unsigned GZ_BUF_SIZE = 4096;
-constexpr char GZ_SUFFIX[] = ".gz";
 constexpr unsigned BUF_SIZE = 1024;
 constexpr unsigned MAX_PROCESS_PATH = 1024;
-constexpr unsigned HASH_FACTOR = 16127;
-constexpr unsigned MILLISEC_OF_PER_SEC = 1000;
-constexpr unsigned MICROSEC_OF_PER_MILLISEC = 1000;
 constexpr int MIN_APP_USERID = 10000;
 constexpr uint32_t MAX_NAME_LENGTH = 4096;
 constexpr int AID_ROOT   = 0;
 constexpr int AID_SYSTEM = 1000;
 constexpr mode_t DEFAULT_LOG_FILE_MODE = 0644;
 constexpr mode_t DEFAULT_LOG_DIR_MODE = 0775;
-constexpr char CUSTOM_SANITIZER_LOG_PATH[] = "/data/log/faultlog/faultlogger/";
-constexpr char ROOT_FAULTLOG_LOG_PATH[] = "/data/log/faultlog/";
 const std::string DEVICE_OHOS_VERSION_PARAM = "hw_sc.build.os.version";
 const std::string EMPTY_PARAM = "";
 
@@ -49,12 +38,9 @@ typedef struct DfxBundleInfo {
     std::string versionName;
 } DfxBundleInfo;
 
-std::vector<std::string> SplitString(const std::string& input, const std::string& regex);
-unsigned HashString(const std::string& input);
 bool IsModuleNameValid(const std::string& name);
 std::string GetApplicationNameById(int32_t uid);
 bool GetDfxBundleInfo(const std::string& bundleName, DfxBundleInfo& bundleInfo);
-void WriteCollectedData(T_SANITIZERD_PARAMS *params);
 } // namespace HiviewDFX
 } // namespace OHOS
 
