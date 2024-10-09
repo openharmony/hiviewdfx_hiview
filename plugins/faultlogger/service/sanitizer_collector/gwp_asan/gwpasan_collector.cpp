6867e51767656e935275f5c42a90072064bda895
mlkgeek <122681168@qq.com>
2024-10-09 11:31:21 +0800

    [Bug]: 涉及ASAN日志落盘方案优化 Issue:#IAVME0 Test:Build & Boot devices Signed-off-by: oumingyuan <oumingyuan@huawei.com>

--- plugins/faultlogger/service/sanitizer_collector/gwp_asan/gwpasan_collector.cpp
+++ plugins/faultlogger/service/sanitizer_collector/gwp_asan/gwpasan_collector.cpp
@@ -102,7 +102,7 @@ void WriteGwpAsanLog(char* buf, size_t sz)
 void ReadGwpAsanRecord(const std::string& gwpAsanBuffer, const std::string& errType)
 {
     GwpAsanCurrInfo currInfo;
-    currInfo.description = gwpAsanBuffer;
+    currInfo.description = ((gwAsanBuffer.size() > ASAN_LOG_SIZE) ? (gwpAsanBuffer.substr(O, ASAN_LOG_SIZE) + "\nEnd Asan report") : gwpAsanBuffer);
     currInfo.pid = getpid();
     currInfo.uid = getuid();
     currInfo.errType = errType;
