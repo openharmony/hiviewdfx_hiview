752aea514569531acd7e3f87d31fa48ba2552dcc
郑冬冬冬 <zhengchengkai@huawei.com>
2024-11-22 13:11:10 +0800

    Message:fix Codes
    Signed-off-by: 郑冬冬冬 <zhengchengkai@huawei.com>

--- plugins/freeze_detector/vendor.cpp
+++ plugins/freeze_detector/vendor.cpp
@@ -29,8 +29,8 @@ namespace {
     static const int MILLISECOND = 1000;
     static const int MAX_LINE_NUM = 100;
     static const int TIME_STRING_LEN = 16;
-    static const int MAX_FILE_NUM = 500;
-    static const int MAX_FOLDER_SIZE = 50 * 1024 * 1024;
+    static const int MAX_FILE_NUM = 5;
+    static const int MAX_FOLDER_SIZE = 5 * 1024 * 1024;
     static constexpr const char* const TRIGGER_HEADER = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
     static constexpr const char* const HEADER = "*******************************************";
     static constexpr const char* const HYPHEN = "-";
