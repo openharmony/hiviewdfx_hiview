1ee43fd908594d045a37d6e83ad5fae51b5f1fbb
negegne <zhengshunxin@huawei.com>
2025-04-01 11:29:22 +0800

    trace aging
    Signed-off-by: negegne <zhengshunxin@huawei.com>

--- interfaces/inner_api/unified_collection/trace_caller.h
+++ interfaces/inner_api/unified_collection/trace_caller.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,16 +24,16 @@ enum TraceCaller {
     RELIABILITY,
     XPERF,
     XPOWER,
-    BETACLUB,
     OTHER,
-    HIVIEW
+    HIVIEW,
+    SCREEN
 };
 
 // Used for ipc client interface, for outside
 enum TraceClient {
     COMMAND,
     COMMON_DEV,
-    APP
+    BETACLUB
 };
 
 // Used for telemetry interface param
