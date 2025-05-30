5a9c250b9ef6188494bbddfefc1b15f32f3c758a
huanglihao <huanglihao3@huawei.com>
2025-05-30 09:46:47 +0000

    规范
    Signed-off-by: huanglihao <huanglihao3@huawei.com>

--- plugins/performance/perfmonitor/common/event_builder/xperf_event.h
+++ plugins/performance/perfmonitor/common/event_builder/xperf_event.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #ifndef XPERF_EVENT_H
 #define XPERF_EVENT_H
 
