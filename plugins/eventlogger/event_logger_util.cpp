32e65b33bef41a6fee461bbf54352a08223c1b1c
hhl <yinhuiling@h-partners.com>
2025-06-17 19:30:29 +0800

    fix issues
    Signed-off-by: hhl <yinhuiling@h-partners.com>

--- plugins/eventlogger/event_logger_util.cpp
+++ plugins/eventlogger/event_logger_util.cpp
@@ -20,12 +20,15 @@
 #include "file_util.h"
 #include "time_util.h"
 #include "string_util.h"
+#include "log_file.h"
 
 namespace OHOS {
 namespace HiviewDFX {
 namespace {
     static const int LOG_MAP_SIZE = 2;
     static constexpr time_t FORTYEIGHT_HOURS = 48 * 60 * 60;
+    static const size_t FREEZE_FILE_NAME_SIZE = 6;
+    static const int FREEZE_UID_INDEX = 4;
     static constexpr const char* const APPFREEZE = "appfreeze";
     static constexpr const char* const SYSFREEZE = "sysfreeze";
     static constexpr const char* const SYSWARNING = "syswarning";
@@ -135,6 +138,34 @@ void StartBootScan()
         HIVIEW_LOGI("Boot scan file: %{public}s.", file.c_str());
         AddFaultLog(info);
     }
+}
+
+int32_t GetUidFromFileName(const std::string& fileName)
+{
+    std::vector<std::string> splitStr;
+    StringUtil::SplitStr(fileName, "-", splitStr);
+    int32_t id = 0;
+    if (splitStr.size() == FREEZE_FILE_NAME_SIZE) {
+        StringUtil::ConvertStringTo<int32_t>(splitStr[FREEZE_UID_INDEX], id);
+    }
+    return id;
+}
+
+LogStoreEx::LogFileFilter CreateLogFileFilter(int32_t id, const std::string& filePrefix)
+{
+    LogStoreEx::LogFileFilter filter = [id, filePrefix](const LogFile &file) {
+        if (file.name_.find(filePrefix) == std::string::npos) {
+            return false;
+        }
+
+        int fileId = GetUidFromFileName(file.name_);
+        if (fileId != id) {
+            return false;
+        }
+
+        return true;
+    };
+    return filter;
 }
 } // namespace HiviewDFX
 } // namespace OHOS
