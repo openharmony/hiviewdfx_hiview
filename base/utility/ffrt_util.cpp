fe459d3a6a9a52c9adc48dc7ca5f251ba77e3175
xuyong <xuyong59@huawei.com>
2025-06-30 23:38:40 +0800

    optimize the logic of event export
    Signed-off-by: xuyong <xuyong59@huawei.com>

--- base/utility/ffrt_util.cpp
+++ base/utility/ffrt_util.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -22,18 +22,21 @@ namespace OHOS {
 namespace HiviewDFX {
 namespace FfrtUtil {
 namespace {
-constexpr int64_t CHECK_CYCLE_SECONDS = 10; // 10 seconds
+constexpr int64_t CHECK_CYCLE_SECONDS = 60; // 60 seconds
 
-int64_t GetCurrentSecond()
+inline int64_t GetCurrentSecond()
 {
     return static_cast<int64_t>(TimeUtil::GetSteadyClockTimeMs()) / TimeUtil::SEC_TO_MILLISEC;
 }
 }
 
+
 void Sleep(int64_t taskCycleSec)
 {
     int64_t lastTimeSec = GetCurrentSecond();
     while (std::abs(GetCurrentSecond() - lastTimeSec) < taskCycleSec) {
+        // avoid too long time to sleep, check whether the time difference
+        // is exceed the task cycle is neccessary after each 60 seconds' sleep
         ffrt::this_task::sleep_for(std::chrono::seconds(CHECK_CYCLE_SECONDS));
     }
 }
