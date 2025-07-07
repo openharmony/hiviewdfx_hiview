d8d2c7e0f310976a94aec385d138781f81d60911
x00503640 <xufang21@huawei.com>
2025-07-07 21:59:05 +0800

    app list fling repose skip
    Signed-off-by: x00503640 <xufang21@huawei.com>

--- plugins/performance/perfmonitor/src/input_monitor.cpp
+++ plugins/performance/perfmonitor/src/input_monitor.cpp
@@ -46,7 +46,8 @@ void InputMonitor::RecordInputEvent(PerfActionType type, PerfSourceType sourceTy
             {
                 XPERF_TRACE_SCOPED("RecordInputEvent: last_up=%lld(ns)", static_cast<long long>(time));
                 mInputTime[LAST_UP] = time;
-                SceneMonitor::GetInstance().OnSceneChanged(SceneType::APP_RESPONSE, true);
+                SceneMonitor::GetInstance().OnSceneChanged(SceneType::APP_RESPONSE, true,
+                    SceneMonitor::GetInstance().GetCurrentSceneId());
                 break;
             }
         case FIRST_MOVE:
