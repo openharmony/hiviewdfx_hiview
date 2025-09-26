a0f853b7c44b9654eab82ffd52a2e5167a2d807f
zhengdongdong12 <zhengchengkai@huawei.com>
2025-09-26 11:12:04 +0800

    update: 更新文件 faultlog_event_base.cpp
    Signed-off-by: zhengdongdong12 <zhengchengkai@huawei.com>

--- plugins/faultlogger/service/fault_event/base/faultlog_event_base.cpp
+++ plugins/faultlogger/service/fault_event/base/faultlog_event_base.cpp
@@ -51,7 +51,8 @@ void FaultLogEventBase::UpdateSysEvent(SysEvent& sysEvent, FaultLogInfo& info)
     sysEvent.SetEventValue(FaultKey::VERSION_CODE, info.sectionMap[FaultKey::VERSION_CODE]);
     sysEvent.SetEventValue(FaultKey::PRE_INSTALL, info.sectionMap[FaultKey::PRE_INSTALL]);
     sysEvent.SetEventValue(FaultKey::FOREGROUND, info.sectionMap[FaultKey::FOREGROUND]);
-    sysEvent.SetEventValue(FaultKey::DEVICE_MODE, strtol(info.sectionMap[FaultKey::DEVICE_MODE].c_str(), nullptr, FaultLogger::DECIMAL_BASE));
+    sysEvent.SetEventValue(FaultKey::DEVICE_MODE, 
+        strtol(info.sectionMap[FaultKey::DEVICE_MODE].c_str(), nullptr, FaultLogger::DECIMAL_BASE));
 
     std::map<std::string, std::string> eventInfos;
     if (AnalysisFaultlog(info, eventInfos)) {
