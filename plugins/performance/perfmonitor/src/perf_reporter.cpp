9dd92f3a85329ea51264a1db1a011fd2eb6f9170
liuzhengjie1 <liuzhengjie1@huawei.com>
2026-01-22 16:03:38 +0800

    update: 更新文件 perf_reporter.cpp
    Signed-off-by: liuzhengjie1 <liuzhengjie1@huawei.com>

--- plugins/performance/perfmonitor/src/perf_reporter.cpp
+++ plugins/performance/perfmonitor/src/perf_reporter.cpp
@@ -521,8 +521,8 @@ void EventReporter::ReportJankFrameUnFiltered(JankInfo& info)
     XperfEventReporter reporter;
     reporter.Report(ACE_DOMAIN, event);
     XPERF_TRACE_SCOPED("JANK_FRAME_UNFILTERED: skipppedFrameTime=%lld(ms), windowName=%s, filterType=%" PRIu64
-        ",vsyncTime=%lld(ns)", static_cast<long long>(skippedFrameTime / NS_TO_MS), windowName.c_str(),
-        sceneTag, static_cast<long long>(vsyncTime));
+        ",vsyncTime=%lld(ns), pageName=%s, timestamp=%lld(ns)", static_cast<long long>(skippedFrameTime / NS_TO_MS), windowName.c_str(),
+        sceneTag, static_cast<long long>(vsyncTime), pageName.c_str(), static_cast<long long>(GetCurrentRealTimeNs())) ;
 }
 
 #ifdef RESOURCE_SCHEDULE_SERVICE_ENABLE
