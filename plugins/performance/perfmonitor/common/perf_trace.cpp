d128d1c6d7897e0f6ad3c45f7330178cc9d0473a
liuzhengjie1 <liuzhengjie1@huawei.com>
2026-01-23 16:42:48 +0800

    update: 更新文件 perf_trace.cpp
    Signed-off-by: liuzhengjie1 <liuzhengjie1@huawei.com>

--- plugins/performance/perfmonitor/common/perf_trace.cpp
+++ plugins/performance/perfmonitor/common/perf_trace.cpp
@@ -42,7 +42,7 @@ void FormatTraceName(char *name, size_t size, const char *fmt, ...)
     va_start(args, fmt);
     int ret = vsnprintf_s(name, size, size - 1, fmt, args);
     va_end(args);
-    if (ret == -1){
+if (ret == -1) {
         return;
     }
 }
