1804275a8f2aa90952cad90ca34d5dcc0e261a6e
SXWL_ <bokaibin@huawei.com>
2026-04-19 15:47:31 +0800

    optimise load complete monitor
    Signed-off-by: SXWL_ <bokaibin@huawei.com>

---
+++ plugins/performance/perfmonitor/load_complete/src/collect_strategy_factory.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "collect_strategy_factory.h"
+#include <unordered_map>
+#include <unordered_set>
+
+namespace OHOS {
+namespace HiviewDFX {
+
+std::unique_ptr<ComponentCollectStrategy> CollectStrategyFactory::CreateStrategy(
+    const std::string& bundleName, 
+    bool isLaunch
+) {
+    // 非启动场景使用非预加载模式
+    if (!isLaunch) {
+        return std::make_unique<NonPreloadCollectStrategy>();
+    }
+    
+    // 不检测的应用（直接结束不上报）
+    // 抖音和快手 采用起播作为加载完成的标志
+    static const std::unordered_set<std::string> noDetectApps = {
+        "com.ss.hm.ugc.aweme",  // 抖音
+        "com.kuaishou.hmapp"    // 快手
+    };
+    
+    if (noDetectApps.find(bundleName) != noDetectApps.end()) {
+        return std::make_unique<NoDetectCollectStrategy>();
+    }
+    
+    // 启动场景的应用是否预加载
+    static const std::unordered_map<std::string, bool> bundleConfigs = {
+        {"yylx.danmaku.bili", true},
+        {"com.sankuai.hmeituan", false},
+        {"com.xingin.xhs_hos", true},
+        {"com.jd.hm.mall", true},
+        {"com.tencent.wechat", false},
+        {"com.alipay.mobile.client", false}
+    };
+
+    auto it = bundleConfigs.find(bundleName);
+    bool usePreload = (it != bundleConfigs.end()) ? it->second : false;
+    
+    if (usePreload) {
+        return std::make_unique<PreloadCollectStrategy>();
+    }
+    return std::make_unique<NonPreloadCollectStrategy>();
+}
+
+} // namespace HiviewDFX
+} // namespace OHOS
