b1d538ff2ff483d389847d9b5577d952671d9357
fangzz1 <fangzhizhou@h-partners.com>
2026-04-21 21:00:33 +0800

    update: 更新文件 video_jank_monitor.cpp
    Signed-off-by: fangzz1 <fangzhizhou@h-partners.com>

--- plugins/performance/xperf_service/services/framework/xperf_monitor/src/video_jank_monitor.cpp
+++ plugins/performance/xperf_service/services/framework/xperf_monitor/src/video_jank_monitor.cpp
@@ -12,6 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <thread>
+#include <string>
+#include <sstream>
 #include "xperf_service_log.h"
 #include "video_jank_monitor.h"
 #include "xperf_constant.h"
