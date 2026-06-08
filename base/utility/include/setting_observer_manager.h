7f7a528ce974ea9abdea304574bc45377bec207e
honghecun <xuyong59@huawei.com>
2026-06-08 17:28:13 +0800

    add InsertValue for SettingObserverManager
    Signed-off-by: honghecun <xuyong59@huawei.com>

--- base/utility/include/setting_observer_manager.h
+++ base/utility/include/setting_observer_manager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -47,9 +47,9 @@ public:
     bool RegisterObserver(const std::string& paramKey, SettingObserver::ObserverCallback callback);
     bool UnregisterObserver(const std::string& paramKey);
     std::string GetStringValue(const std::string& paramKey, const std::string& defaultVal = "");
+    int SetStringValue(const std::string& paramKey, const std::string& paramVal);
 
 private:
-    Uri AssembleUri(const std::string& paramKey);
     sptr<SettingObserver> GetSettingObserver(const std::string& paramKey);
 
 private:
