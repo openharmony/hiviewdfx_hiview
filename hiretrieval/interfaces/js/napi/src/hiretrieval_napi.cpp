0d66a712e99c090b797157e808f6578860dbe0ae
honghecun9zu <xuyong59@huawei.com>
2026-08-17 11:23:29 +0800

    update: 更新文件 hiretrieval_napi.cpp
    Signed-off-by: honghecun9zu <xuyong59@huawei.com>

--- hiretrieval/interfaces/js/napi/src/hiretrieval_napi.cpp
+++ hiretrieval/interfaces/js/napi/src/hiretrieval_napi.cpp
@@ -40,7 +40,7 @@ static napi_value Init(napi_env env, napi_callback_info info)
 {
     int ret = HiRetrievalMgr::GetInstance().Init();
     HiRetrievalNapiUtil::CheckRetAndThrowError(env, ret);
-    return nullptr;napi_value val = nullptr;
+    napi_value val = nullptr;
     HiRetrievalNapiUtil::CreateJsUndefinedValue(env, val);
     return val;
 }
