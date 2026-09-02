cb04e15d2982b6544a06a4ea1289a561d7e5230f
zhou_xq <zhouxiaoqin4@h-partners.com>
2026-09-02 19:33:25 +0800

    AI clean code modify
    Signed-off-by: zhou_xq <zhouxiaoqin4@h-partners.com>

--- base/event_publish/include/event_publish.h
+++ base/event_publish/include/event_publish.h
@@ -51,7 +51,7 @@ private:
     EventPublish();
     ~EventPublish();
     class Impl;
-    std::unique_ptr<Impl> impl_;
+    std::shared_ptr<Impl> impl_;
 };
 } // namespace HiviewDFX
 } // namespace OHOS
