20dd5cb22152b1fcd9b7940b9ec905cdf6242453
liziling-lalalala <liziling4@huawei.com>
2026-09-21 11:49:56 +0800

    update: 更新文件 panic_unittest.cpp
    Signed-off-by: liziling-lalalala <liziling4@huawei.com>

--- utility/test/unittest/panic_unittest.cpp
+++ utility/test/unittest/panic_unittest.cpp
@@ -181,5 +181,47 @@ HWTEST(SmartParserPanicTest, SmartParserPanicTest004, testing::ext::TestSize.Lev
     ASSERT_EQ(eventInfos["SECOND_FRAME"], "syscall_entry+0x4708/0xb61c");
     ASSERT_EQ(eventInfos["LAST_FRAME"], "__el0_sync+0x2a4/0x2b0");
 }
+
+/**
+ * @tc.name: SmartParserPanicTest005
+ * @tc.desc: Smart parser hm kernel panic log.
+ * @tc.type: FUNC
+ */
+HWTEST(SmartParserPanicTest, SmartParserPanicTest005, testing::ext::TestSize.Level3)
+{
+    /**
+     * @tc.steps: step1. parser log
+     */
+    const std::string logPath = std::string{LogUtil::SMART_PARSER_TEST_DIR}  +
+        "/SmartParserPanicTest005/19700101000000-00000007/ap_log/dmesg-ramoops-0";
+    const std::string eventType = "PANIC";
+    auto eventInfos = SmartParser::Analysis(logPath, SMART_PARSER_PATH, eventType);
+
+    /**
+     * @tc.steps: step2. check result
+     */
+    std::string endStack = "<dump_backtrace+0x0/0x8>\n";
+    endStack += "<show_stack+0x24/0x30>\n";
+    endStack += "<dump_stack+0xc0/0x11c>\n";
+    endStack += "<panic+0x178/0x3f0>\n";
+    endStack += "<sysrq_handle_term+0x0/0x38>\n";
+    endStack += "<__handle_sysrq+0x170/0x1b8>\n";
+    endStack += "<write_sysrq_trigger+0xb4/0xe0>\n";
+    endStack += "<proc_reg_write+0x90/0x1a0>\n";
+    endStack += "<vfs_write+0x14c/0x410>\n";
+    endStack += "<ksys_write+0x84/0xf4>\n";
+    endStack += "<__arm64_sys_write+0x28/0x34>\n";
+    endStack += "<el0_svc_common+0x134/0x22c>\n";
+    endStack += "<el0_svc_compat+0x1c/0x28>\n";
+    endStack += "<el0_sync_compat_handler+0xc0/0xf0>\n";
+    endStack += "<el0_sync_compat+0x184/0x1c0>\n";
+
+    ASSERT_EQ(eventInfos["END_STACK"], endStack);
+
+    Tbox::FilterTrace(eventInfos, eventType);
+    ASSERT_EQ(eventInfos["FIRST_FRAME"], "sysrq_handle_term+0x0/0x38");
+    ASSERT_EQ(eventInfos["SECOND_FRAME"], "__handle_sysrq+0x170/0x1b8");
+    ASSERT_EQ(eventInfos["LAST_FRAME"], "el0_sync_compat+0x184/0x1c0");
+}
 } // namespace HiviewDFX
 } // namespace OHOS
